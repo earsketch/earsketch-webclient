3a1fb5f11c30781252d937c6c7319640448bfe94
Erin's Desktop <nern.strudel@gmail.com>
2021-01-06 11:53:07 -0500

    trying to fix

--- scripts/src/app/services/caiAnalysisModule.js
+++ scripts/src/app/services/caiAnalysisModule.js
@@ -5,7 +5,7 @@ import {AUDIOKEYS_RECOMMENDATIONS} from 'audiokeysRecommendations';
  *
  * @author Jason Smith
  */
-app.factory('caiAnalysisModule', ['esconsole', 'complexityCalculator', 'recommender', 'audioLibrary', 'ESUtils', 'caiStudent', function (esconsole, complexityCalculator, recommender, audioLibrary, ESUtils, caiStudent) {
+app.factory('caiAnalysisModule', ['esconsole', 'complexityCalculator', 'recommender', 'audioLibrary', 'ESUtils',  function (esconsole, complexityCalculator, recommender, audioLibrary, ESUtils) {
 
   var librarySounds = [];
   var librarySoundGenres = [];
