3a41363ba350d187073a5c37e16ebb5a21bb5988
Stephen Garrett <manodrum@gmail.com>
2021-04-08 17:04:52 -0400

    clean up commented code

--- scripts/src/app/tabController.js
+++ scripts/src/app/tabController.js
@@ -681,11 +681,6 @@ app.controller("tabController", ['$rootScope', '$scope', '$http', '$uibModal', '
         } else {
             userProject.importScript(script).then(function(importedScript) {
                 userNotification.show('Imported a copy of script "' + script.name + '" (created by ' + author + ') into your local Script Browser.');
-                // For some reason, closing is only required for read-only script
-
-                // if (!script.isShared) {
-                //     $scope.closeTab($scope.getTabId(script.shareid));
-                // }
 
                 $ngRedux.dispatch(tabs.closeTab(script.shareid));
 
