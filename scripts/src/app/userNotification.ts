c38e6a6dae67adaaf380861ee42a81f296eb60c9
Ian Clester <ijc@ijc8.me>
2021-05-21 16:20:10 -0400

    Cleanup, add more types.

--- scripts/src/app/userNotification.ts
+++ scripts/src/app/userNotification.ts
@@ -1,15 +1,32 @@
 import * as helpers from '../helpers'
 
+// TODO: Once we port the notification UI to React, we should probably move this state to Redux.
+
+interface Notification {
+    message: { text: string, json?: string, action?: string }
+    notification_type: string
+    time: number
+    unread: boolean
+    pinned: boolean
+    // Collaboration data.
+    sender?: string
+    script_name?: string
+    shareid?: string
+    id?: string
+    created?: number
+}
+
 // TODO: separate the temporary and permanent notification services?
 export const queue = []
-export let history: any[] = []
+export let history: Notification[] = []
 
 export const user = { role: "", loginTime: Date.now() }
 
 export const callbacks = {
-    show: (...args: any[]) => {},
+    show: (text: string, type: string="") => {},
     hide: () => {},
-    popup: (...args: any[]) => {},
+    popup: (text: string, type: string="", duration: number | undefined=undefined) => {},
+
     hidePopup: () => {},
     addSharedScript: (shareID: string, id: string) => {},
 }
@@ -21,27 +38,22 @@ export const state = {
 
 export const popupQueue = []
 
-export function show(text: string, type: string="", ...args: any[]) {
+export const show = (text: string, type: string="", duration: number | undefined=undefined) => {
     // check type for registering to the notification history
     // TODO: handle with proper message types defined
-    if (['bell','popup','history'].indexOf(type)) { // temporary tags for bell-icon dropdown notifications
+    if (['bell','popup','history'].includes(type)) { // temporary tags for bell-icon dropdown notifications
         history.unshift({
             message: {
                 text
             },
             notification_type: type,
             time: Date.now(),
-            read: false,
+            unread: false,
             pinned: false
         })
     } else if (type === 'editProfile') {
-        while (history.some(function (item) {
-                return item.notification_type === 'editProfile'
-            })) {
-
-            var index = history.findIndex(function (item) {
-                return item['notification_type'] === 'editProfile'
-            })
+        while (history.some(item => item.notification_type === 'editProfile')) {
+            const index = history.findIndex(item => item['notification_type'] === 'editProfile')
 
             if (index !== -1) {
                 history.splice(index, 1)
@@ -66,44 +78,37 @@ export function show(text: string, type: string="", ...args: any[]) {
     } else {
         // showCallback.apply(this, arguments)
     }
-    callbacks.popup.apply(this, [text, type, ...args])
+    callbacks.popup(text, type, duration)
 }
 
-export const showBanner = (...args: any[]) => callbacks.show.apply(this, args)
+export const showBanner = (text: string, type: string="") => callbacks.show(text, type)
 
 export const hide = () => callbacks.hide()
 export const hidePopup = () => callbacks.hidePopup()
 
 // Only show the latest broadcast on the client side.
-function truncateBroadcast() {
-    var nonBroadcasts = history.filter(function (v) {
-        return v.notification_type !== 'broadcast'
-    })
-
-    var latestBroadcast = history.filter(function (v) {
-        return v.notification_type === 'broadcast'
-    })[0]
+const truncateBroadcast = () => {
+    const nonBroadcasts = history.filter(v => v.notification_type !== 'broadcast')
+    const latestBroadcast = history.find(v => v.notification_type === 'broadcast')
 
     history = nonBroadcasts
 
-    if (typeof(latestBroadcast) !== 'undefined') {
+    if (typeof latestBroadcast !== 'undefined') {
         history.unshift(latestBroadcast)
     }
 }
 
 // Fill the history array at initialization from webservice call as well as localStorage. Sorting might be needed.
-export const loadHistory = (notificationList: any[]) => {
+export const loadHistory = (notificationList: Notification[]) => {
     let text = ''
     history = notificationList
 
     // filter out 'teacherBroadcast' messages
-    if (['teacher', 'admin'].indexOf(user.role) === -1) {
-        history = history.filter(function (v) {
-            return v.notification_type !== 'teacher_broadcast'
-        })
+    if (!['teacher', 'admin'].includes(user.role)) {
+        history = history.filter(v => v.notification_type !== 'teacher_broadcast')
     }
 
-    history = history.map(function (v) {
+    history = history.map(v => {
         v.pinned = (v.notification_type === 'broadcast' || v.notification_type === 'teacher_broadcast')
 
         // TODO: notification_type is too verbose
@@ -114,10 +119,10 @@ export const loadHistory = (notificationList: any[]) => {
 
             // auto-add new view-only scripts that are shared to the shared-script browser
             if (v.unread) {
-                callbacks.addSharedScript(v.shareid, v.id)
+                callbacks.addSharedScript(v.shareid!, v.id!)
             }
         } else if (v.notification_type === 'collaborate_script') {
-            var data = JSON.parse(v.message.json)
+            const data = JSON.parse(v.message.json!)
             // $rootScope.$emit('notificationHistoryLoaded', data); // trigger subscribed callbacks
 
             // received only by the ones affected
@@ -144,8 +149,7 @@ export const loadHistory = (notificationList: any[]) => {
             v.message.text = '[Teacher] ' + v.message.text
         }
 
-        v.time = new Date(v.created)
-        console.log(v.created, v.time)
+        v.time = v.created!
 
         // hack around only receiving notification history (collection) but not individual messages
         // TODO: always send individual notification from server
@@ -156,42 +160,38 @@ export const loadHistory = (notificationList: any[]) => {
         return v
     })
 
-    history.sort(function (a, b) {
-        return b.time - a.time
-    })
+    history.sort((a, b) => b.time - a.time)
 
     truncateBroadcast()
     scopeDigest()
 }
 
-export const clearHistory = function () {
-    history = []
-}
+export const clearHistory = () => history = []
 
 // TODO: should receive notification collection here as well
-export const handleBroadcast = (data: any) => {
+export const handleBroadcast = (data: Notification) => {
     show('From EarSketch team: ' + data.message.text, 'broadcast')
-    data.time = new Date()
+    data.time = Date.now()
     data.pinned = true
     history.unshift(data)
     truncateBroadcast()
     scopeDigest()
 }
 
-export const handleTeacherBroadcast = (data: any) => {
+export const handleTeacherBroadcast = (data: Notification) => {
     if (user.role !== 'teacher') {
         return
     }
 
     show('From EarSketch team to teachers: ' + data.message.text, 'broadcast')
-    data.time = new Date()
+    data.time = Date.now()
     data.pinned = true
     history.unshift(data)
     // truncateBroadcast()
     scopeDigest()
 }
 
-function scopeDigest() {
+const scopeDigest = () => {
     const $rootScope = helpers.getNgRootScope()
     $rootScope.$broadcast('notificationsUpdated')
     $rootScope.$applyAsync()
