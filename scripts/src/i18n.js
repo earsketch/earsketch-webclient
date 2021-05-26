3b2cc39e1c0951c8fe6d13e2aed0476cd206eb72
Stephen Garrett <manodrum@gmail.com>
2021-05-26 17:25:50 -0400

    remove comments

--- scripts/src/i18n.js
+++ scripts/src/i18n.js
@@ -3,21 +3,6 @@ import { initReactI18next } from "react-i18next";
 import en from './locales/en/common.json';
 import es from './locales/es/common.json';
 
-// the translations
-// (tip move them in a JSON file and import them)
-// const resources = {
-//     en: {
-//         translation: {
-//             "Welcome to React": "Welcome to React and react-i18next"
-//         }
-//     },
-//     fr: {
-//         translation: {
-//             "Welcome to React": "Bienvenue à React et react-i18next"
-//         }
-//     }
-// };
-
 i18n
     .use(initReactI18next) // passes i18n down to react-i18next
     .init({
