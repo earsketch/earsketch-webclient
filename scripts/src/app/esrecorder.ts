e502f5dcca55e008281c22303d910c1c91ae8e2d
Ian Clester <ijc@ijc8.me>
2021-06-02 16:33:59 -0400

    Don't duplicate code from renderer.
    Actually, it looks like encodeWAV & co. are originally from recorderjs.
    We should either drop that dependency or avoid duplicating it.

--- scripts/src/app/esrecorder.ts
+++ scripts/src/app/esrecorder.ts
@@ -6,6 +6,7 @@ import * as audioLibrary from './audiolibrary'
 import ctx from './audiocontext'
 import * as ESUtils from '../esutils'
 import * as helpers from '../helpers'
+import { encodeWAV } from './renderer'
 import * as userProject from './userProject'
 
 var audioRecorder: any, meter: any, micGain: any, zeroGain: any, previewBs: any, startTime: any, metroOsc: any, beatBuffSrc: any, eventBuffSrc: any;
@@ -380,62 +381,6 @@ function gotBuffer(buf: any) {
     var blob = new Blob([view], {type: 'audio/wav'});
     doneEncoding(blob);
 }
-
-function floatTo16BitPCM(output: DataView, offset: number, input: Float32Array){
-    for (var i = 0; i < input.length; i++, offset+=2){
-        var s = Math.max(-1, Math.min(1, input[i]));
-        output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
-    }
-}
-
-function writeString(view: DataView, offset: number, string: string){
-    for (var i = 0; i < string.length; i++){
-        view.setUint8(offset + i, string.charCodeAt(i));
-    }
-}
-
-function encodeWAV(samples: Float32Array){
-    var buffer = new ArrayBuffer(44 + samples.length * 2);
-    var view = new DataView(buffer);
-
-    var numChannels = 1;
-    var sampleRate = ctx.sampleRate;
-
-    /* RIFF identifier */
-    writeString(view, 0, 'RIFF');
-    /* RIFF chunk length */
-    view.setUint32(4, 36 + samples.length * 2, true);
-    /* RIFF type */
-    writeString(view, 8, 'WAVE');
-    /* format chunk identifier */
-    writeString(view, 12, 'fmt ');
-    /* format chunk length */
-    view.setUint32(16, 16, true);
-    /* sample format (raw) */
-    view.setUint16(20, 1, true);
-    /* channel count */
-    view.setUint16(22, numChannels, true);
-    /* sample rate */
-    view.setUint32(24, sampleRate, true);
-    /* byte rate (sample rate * block align) */
-    view.setUint32(28, sampleRate * 4, true);
-    /* block align (channel count * bytes per sample) */
-    view.setUint16(32, numChannels * 2, true);
-    /* bits per sample */
-    view.setUint16(34, 16, true);
-    /* data chunk identifier */
-    writeString(view, 36, 'data');
-    /* data chunk length */
-    view.setUint32(40, samples.length * 2, true);
-
-    floatTo16BitPCM(view, 44, samples);
-
-    return view;
-}
-
-//this.properties.save = function () {
-//    audioRecorder.exportWAV(doneEncoding);
-//};
 
 function doneEncoding(blob: any) {
     blob.lastModifiedDate = new Date();
