279126719d49b0aa007fedc6559ab18d7a6e6b76
Stephen Garrett <manodrum@gmail.com>
2021-07-01 16:51:22 -0400

    4 space indents

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,37 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules: {
-    'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': ['error'],
-    'space-before-function-paren': ['error', { named: 'never' }],
-    'comma-dangle': ['error', 'always-multiline'],
-  },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        'no-undef': 'off',
-      },
+    extends: [
+        'plugin:react/recommended',
+        'standard',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
     },
-  ],
+    plugins: [
+        'react',
+        '@typescript-eslint',
+    ],
+    rules: {
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': ['error'],
+        'space-before-function-paren': ['error', { named: 'never' }],
+        'comma-dangle': ['error', 'always-multiline'],
+        indent: ['error', 4],
+    },
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            rules: {
+                'no-undef': 'off',
+            },
+        },
+    ],
 }
