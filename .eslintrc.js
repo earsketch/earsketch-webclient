bfff9133d210419422e887671c8043b6bf473b50
Stephen Garrett <manodrum@gmail.com>
2021-07-01 17:18:18 -0400

    double quotes!

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,33 +4,35 @@ module.exports = {
         es2021: true,
     },
     extends: [
-        'plugin:react/recommended',
-        'standard',
+        "plugin:react/recommended",
+        "standard",
     ],
-    parser: '@typescript-eslint/parser',
+    parser: "@typescript-eslint/parser",
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
         },
         ecmaVersion: 12,
-        sourceType: 'module',
+        sourceType: "module",
     },
     plugins: [
-        'react',
-        '@typescript-eslint',
+        "react",
+        "@typescript-eslint",
     ],
     rules: {
-        'no-use-before-define': 'off',
-        '@typescript-eslint/no-use-before-define': ['error'],
-        'space-before-function-paren': ['error', { named: 'never' }],
-        'comma-dangle': ['error', 'always-multiline'],
-        indent: ['error', 4],
+        "no-use-before-define": "off",
+        "@typescript-eslint/no-use-before-define": ["error"],
+        "space-before-function-paren": ["error", { named: "never" }],
+        "comma-dangle": ["error", "always-multiline"],
+        indent: ["error", 4],
+        quotes: ["error", "double", { avoidEscape: true }],
+        "jsx-quotes": ["error", "prefer-double"],
     },
     overrides: [
         {
-            files: ['*.ts', '*.tsx'],
+            files: ["*.ts", "*.tsx"],
             rules: {
-                'no-undef': 'off',
+                "no-undef": "off",
             },
         },
     ],
