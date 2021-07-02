b4b95373cecf30b646d2655848f68b713cac8a0d
Stephen Garrett <manodrum@gmail.com>
2021-07-02 14:36:18 -0400

    eslint fix localeSelector

--- scripts/src/top/LocaleSelector.tsx
+++ scripts/src/top/LocaleSelector.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import { Menu } from '@headlessui/react'
+import React from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Menu } from "@headlessui/react"
 
-import { useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next"
 
-import * as appState from '../app/appState';
-import * as curriculumState from "../browser/curriculumState";
+import * as appState from "../app/appState"
+import * as curriculumState from "../browser/curriculumState"
 
 interface locale {
     displayText: string;
@@ -13,18 +13,18 @@ interface locale {
 }
 
 const AVAILABLE_LOCALES: locale[] = [
-        {displayText: "English", localeCode: "en"},
-        {displayText: "Español", localeCode: "es"}
-    ];
+    { displayText: "English", localeCode: "en" },
+    { displayText: "Español", localeCode: "es" },
+]
 
 export const LocaleSelector = () => {
-    const dispatch = useDispatch();
-    const { i18n } = useTranslation();
+    const dispatch = useDispatch()
+    const { i18n } = useTranslation()
     const curriculumLocation = useSelector(curriculumState.selectCurrentLocation)
 
     const changeLanguage = (lng: string) => {
-        i18n.changeLanguage(lng);
-        dispatch(appState.setLocale(lng));
+        i18n.changeLanguage(lng)
+        dispatch(appState.setLocale(lng))
         dispatch(curriculumState.fetchLocale({ location: curriculumLocation }))
     }
 
@@ -40,22 +40,22 @@ export const LocaleSelector = () => {
                 <Menu.Items className="absolute z-50 right-0 mt-2 origin-top-right bg-gray-100 divide-y divide-gray-100 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     {
                         AVAILABLE_LOCALES.map((locale) =>
-                        <Menu.Item key={locale.localeCode}>
-                            {({ active }) => (
-                                <button
-                                className={`${
-                                active ? 'bg-gray-500 text-white' : 'text-gray-900'
-                                } group flex items-center w-full px-2 py-2`}
-                                onClick={() => changeLanguage(locale.localeCode)}
-                                >
-                                    {locale.displayText}
-                                </button>
-                            )}
-                        </Menu.Item>
+                            <Menu.Item key={locale.localeCode}>
+                                {({ active }) => (
+                                    <button
+                                        className={`${
+                                            active ? "bg-gray-500 text-white" : "text-gray-900"
+                                        } group flex items-center w-full px-2 py-2`}
+                                        onClick={() => changeLanguage(locale.localeCode)}
+                                    >
+                                        {locale.displayText}
+                                    </button>
+                                )}
+                            </Menu.Item>,
                         )
                     }
                 </Menu.Items>
             </Menu>
         </div>
-    );
+    )
 }
