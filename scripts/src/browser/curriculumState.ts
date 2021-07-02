80f67a2e3e7ec8c26db756e11b89fddd838c9cb2
Stephen Garrett <manodrum@gmail.com>
2021-07-01 20:11:54 -0400

    eslint fix curriculumState

--- scripts/src/browser/curriculumState.ts
+++ scripts/src/browser/curriculumState.ts
@@ -1,23 +1,23 @@
-import { createSlice, createAsyncThunk, createSelector } from '@reduxjs/toolkit'
-import lunr from 'lunr'
+import { createSlice, createAsyncThunk, createSelector } from "@reduxjs/toolkit"
+import lunr from "lunr"
 
-import esconsole from '../esconsole'
+import esconsole from "../esconsole"
 import { importScript } from "../ide/IDE"
-import * as layout from '../layout/layoutState'
-import { RootState, ThunkAPI, AppDispatch } from '../reducers'
+import * as layout from "../layout/layoutState"
+import { RootState, ThunkAPI, AppDispatch } from "../reducers"
 import * as userNotification from "../user/notification"
 
-export const fetchContent = createAsyncThunk<any, any, ThunkAPI>('curriculum/fetchContent', async ({ location, url }, { dispatch, getState }) => {
+export const fetchContent = createAsyncThunk<any, any, ThunkAPI>("curriculum/fetchContent", async ({ location, url }, { dispatch, getState }) => {
     const state = getState()
-    const {href: _url, loc: _location} = fixLocation(url, location)
-    dispatch(loadChapter({location: _location}))
+    const { href: _url, loc: _location } = fixLocation(url, location)
+    dispatch(loadChapter({ location: _location }))
     // Check cache before fetching.
-    if (state.curriculum.contentCache[_location.join(',')] !== undefined) {
-        esconsole(`${_location} is in the cache, nothing else to do.`, 'debug')
+    if (state.curriculum.contentCache[_location.join(",")] !== undefined) {
+        esconsole(`${_location} is in the cache, nothing else to do.`, "debug")
         return {}
     }
-    const urlWithoutAnchor = _url.split('#', 1)[0]
-    esconsole(`${_location} not in cache, fetching ${urlWithoutAnchor}.`, 'debug')
+    const urlWithoutAnchor = _url.split("#", 1)[0]
+    esconsole(`${_location} not in cache, fetching ${urlWithoutAnchor}.`, "debug")
     const response = await fetch(urlWithoutAnchor)
     // Add artificial latency; useful for testing:
     // await new Promise(r => setTimeout(r, 1000))
@@ -28,13 +28,13 @@ const processContent = (location: number[], html: string, dispatch: AppDispatch)
     const doc = new DOMParser().parseFromString(html, "text/html")
 
     // Bring these nodes into our document context, but replace the <body> with a <div>.
-    const root = document.createElement('div')
+    const root = document.createElement("div")
     while (doc.body.firstChild) {
         root.appendChild(document.adoptNode(doc.body.firstChild))
     }
 
     // Highlight code blocks.
-    root.querySelectorAll('pre code').forEach(block => hljs.highlightBlock(block))
+    root.querySelectorAll("pre code").forEach(block => hljs.highlightBlock(block))
 
     // Connect copy buttons.
     root.querySelectorAll(".copy-btn-python,.copy-btn-javascript").forEach((button: HTMLButtonElement) => {
@@ -45,7 +45,7 @@ const processContent = (location: number[], html: string, dispatch: AppDispatch)
     root.querySelectorAll('a[href^="#"]').forEach((el: HTMLLinkElement) => {
         el.onclick = (e) => {
             e.preventDefault()
-            dispatch(fetchContent({ url: locationToUrl[location.slice(0, 2).join(',')] + el.getAttribute("href") }))
+            dispatch(fetchContent({ url: locationToUrl[location.slice(0, 2).join(",")] + el.getAttribute("href") }))
         }
     })
     root.querySelectorAll('a[data-es-internallink="true"]').forEach((el: HTMLLinkElement) => {
@@ -85,22 +85,22 @@ const processContent = (location: number[], html: string, dispatch: AppDispatch)
 
     if (location.length < 3) {
         // No sections, leave as-is.
-        return {[location.join(',')]: root}
+        return { [location.join(",")]: root }
     }
 
     // Chop the chapter up into sections.
-    const sect1 = root.querySelector('div.sect1')
+    const sect1 = root.querySelector("div.sect1")
     const body = sect1 ? sect1.parentNode : null
     // Special case: first section (sect2) should come with the opening blurb (sect1).
     // So, we put the body (with later sections removed) in the first slot, and skip the first sect2 in this for loop.
     const chapterLocation = location.slice(0, 2)
     const map: { [key:string]: any } = {}
-    const sect2 = Array.from(root.querySelectorAll('div.sect2'))
-    for (let [idx, el] of sect2.slice(1).entries()) {
-        map[chapterLocation.concat([idx + 1]).join(',')] = el
+    const sect2 = Array.from(root.querySelectorAll("div.sect2"))
+    for (const [idx, el] of sect2.slice(1).entries()) {
+        map[chapterLocation.concat([idx + 1]).join(",")] = el
         el.remove()
     }
-    map[chapterLocation.concat([0]).join(',')] = body
+    map[chapterLocation.concat([0]).join(",")] = body
     return map
 }
 
@@ -114,9 +114,9 @@ interface CurriculumState {
 }
 
 const curriculumSlice = createSlice({
-    name: 'curriculum',
+    name: "curriculum",
     initialState: {
-        searchText: '',
+        searchText: "",
         showResults: false,
         currentLocation: [0],
         focus: [null, null], // unit, chapter
@@ -134,7 +134,7 @@ const curriculumSlice = createSlice({
             state.showTableOfContents = payload
         },
         toggleFocus(state, { payload }) {
-            let [unitIdx, chIdx] = payload
+            const [unitIdx, chIdx] = payload
             if (chIdx) {
                 if (state.focus[1] === chIdx) {
                     state.focus[1] = null
@@ -161,13 +161,13 @@ const curriculumSlice = createSlice({
     extraReducers: builder => {
         builder.addCase(fetchContent.fulfilled, (state: CurriculumState, action: { payload: any }) => {
             // Update the cache.
-            state.contentCache = {...state.contentCache, ...action.payload}
+            state.contentCache = { ...state.contentCache, ...action.payload }
         })
 
         builder.addCase(fetchContent.rejected, (...args) => {
-            esconsole("Fetch failed! " + JSON.stringify(args), 'error')
+            esconsole("Fetch failed! " + JSON.stringify(args), "error")
         })
-    }
+    },
 })
 
 export default curriculumSlice.reducer
@@ -186,7 +186,7 @@ export const selectShowResults = (state: RootState) => state.curriculum.showResu
 
 export const selectCurrentLocation = (state: RootState) => state.curriculum.currentLocation
 
-export const selectContent = (state: RootState) => state.curriculum.contentCache[state.curriculum.currentLocation.join(',')]
+export const selectContent = (state: RootState) => state.curriculum.contentCache[state.curriculum.currentLocation.join(",")]
 
 export const selectShowTableOfContents = (state: RootState) => state.curriculum.showTableOfContents
 
@@ -196,9 +196,9 @@ export const selectFocus = (state: RootState) => state.curriculum.focus
 const documents = ESCurr_SearchDoc
 
 const idx = lunr(function () {
-    this.ref('id')
-    this.field('title')
-    this.field('text')
+    this.ref("id")
+    this.field("title")
+    this.field("text")
 
     documents.forEach(function (doc) {
         this.add(doc)
@@ -206,15 +206,14 @@ const idx = lunr(function () {
 })
 
 export interface SearchResult {
-    id: lunr.Index.Result['ref'],
+    id: lunr.Index.Result["ref"],
     title: string
 }
 
 export const selectSearchResults = createSelector(
     [selectSearchText],
     (searchText): SearchResult[] => {
-        if (!searchText)
-            return []
+        if (!searchText) { return [] }
         try {
             return idx.search(searchText).map((res) => {
                 // @ts-ignore: TODO: handle not-found cases.
@@ -223,23 +222,23 @@ export const selectSearchResults = createSelector(
                 }).title
                 return {
                     id: res.ref,
-                    title: title
+                    title: title,
                 }
             })
         } catch (error) {
             // Not very concerning; searching for 'foo:', for example, causes a parse error.
-            esconsole(`lunr parse error on "${searchText}"`, 'debug')
+            esconsole(`lunr parse error on "${searchText}"`, "debug")
             return []
         }
-    }
+    },
 )
 
 export const getChNumberForDisplay = (unitIdx: number|string, chIdx: number|string) => {
-    unitIdx = typeof(unitIdx)==='number' ? unitIdx : parseInt(unitIdx)
-    chIdx = typeof(chIdx)==='number' ? chIdx : parseInt(chIdx)
+    unitIdx = typeof (unitIdx) === "number" ? unitIdx : parseInt(unitIdx)
+    chIdx = typeof (chIdx) === "number" ? chIdx : parseInt(chIdx)
     const unit = toc[unitIdx]
     if (unit.chapters && (unit.chapters[chIdx] === undefined || unit.chapters[chIdx].displayChNum === -1)) {
-        return ''
+        return ""
     } else {
         return unit.chapters && unit.chapters[chIdx].displayChNum
     }
@@ -249,12 +248,12 @@ export const selectPageTitle = createSelector(
     [selectCurrentLocation, selectContent],
     (location, content) => {
         if (location[0] === -1) {
-            return 'Table of Contents'
+            return "Table of Contents"
         } else if (content === undefined) {
-            return 'Loading...'
+            return "Loading..."
         }
 
-        let title = ''
+        let title = ""
 
         if (location.length === 1) {
             return toc[location[0]].title
@@ -265,21 +264,21 @@ export const selectPageTitle = createSelector(
             }
             const chNumForDisplay = getChNumberForDisplay(location[0], location[1])
             if (chNumForDisplay) {
-                title = chNumForDisplay + ': ' + title
+                title = chNumForDisplay + ": " + title
             }
             return title
         } else if (location.length === 3) {
             const h3 = content.querySelector("h3")
             if (h3) {
-                title = (+location[2]+1) + ': ' + h3.textContent
+                title = (+location[2] + 1) + ": " + h3.textContent
             }
             const chNumForDisplay = getChNumberForDisplay(location[0], location[1])
             if (chNumForDisplay) {
-                title = chNumForDisplay + '.' + title
+                title = chNumForDisplay + "." + title
             }
             return title
         }
-    }
+    },
 )
 
 export interface TOCItem {
@@ -294,10 +293,10 @@ const toc = ESCurr_TOC as [TOCItem]
 const tocPages = ESCurr_Pages
 
 const locationToPage: { [location:string]: number } = {}
-tocPages.forEach((location, pageIdx) => locationToPage[location.join(',')] = pageIdx)
+tocPages.forEach((location, pageIdx) => locationToPage[location.join(",")] = pageIdx)
 
 export const adjustLocation = (location: number[], delta: number) => {
-    let pageIdx = locationToPage[location.join(',')] + delta
+    let pageIdx = locationToPage[location.join(",")] + delta
     if (pageIdx < 0) {
         pageIdx = 0
     } else if (pageIdx >= tocPages.length) {
@@ -308,7 +307,7 @@ export const adjustLocation = (location: number[], delta: number) => {
 }
 
 export const getURLForLocation = (location: number[]) => {
-    return locationToUrl[location.join(',')];
+    return locationToUrl[location.join(",")]
 }
 
 export function getChapterForError(errorMessage: string) {
@@ -316,7 +315,7 @@ export function getChapterForError(errorMessage: string) {
     const types = ["importerror", "indentationerror", "indexerror", "nameerror", "parseerror", "syntaxerror", "typeerror", "valueerror"]
     let type = errorMessage.split(" ")[3].slice(0, -1).toLowerCase()
     type = aliases[type] || type
-    const anchor = types.includes(type) ? '#' + type : ''
+    const anchor = types.includes(type) ? "#" + type : ""
     return { url: `every-error-explained-in-detail.html${anchor}` }
 }
 
@@ -324,30 +323,30 @@ const urlToLocation: { [key:string]: number[] } = {}
 const locationToUrl: { [key:string]: string } = {}
 toc.forEach((unit: TOCItem, unitIdx: number) => {
     urlToLocation[unit.URL] = [unitIdx]
-    locationToUrl[[unitIdx].join(',')] = unit.URL
+    locationToUrl[[unitIdx].join(",")] = unit.URL
     unit.chapters?.forEach((ch, chIdx) => {
         urlToLocation[ch.URL] = [unitIdx, chIdx]
-        locationToUrl[[unitIdx, chIdx].join(',')] = ch.URL
+        locationToUrl[[unitIdx, chIdx].join(",")] = ch.URL
         ch.sections?.forEach((sec, secIdx) => {
             urlToLocation[sec.URL] = [unitIdx, chIdx, secIdx]
-            locationToUrl[[unitIdx, chIdx, secIdx].join(',')] = sec.URL
+            locationToUrl[[unitIdx, chIdx, secIdx].join(",")] = sec.URL
         })
     })
 })
 
 const fixLocation = (href: string | undefined, loc: number[] | undefined) => {
     if (loc === undefined && href !== undefined) {
-        loc = urlToLocation[href];
+        loc = urlToLocation[href]
     }
 
     if (loc === undefined) {
         // if loc is still undefined then this is a request for an un-indexed page, default them to the welcome page
-        loc = [0];
-        href = undefined as any;
-        userNotification.show('Failed to load curriculum link. Redirecting to welcome page.',"failure2", 2);
+        loc = [0]
+        href = undefined as any
+        userNotification.show("Failed to load curriculum link. Redirecting to welcome page.", "failure2", 2)
     }
 
-   href ??= locationToUrl[loc.join(',')]
+    href ??= locationToUrl[loc.join(",")]
 
     if (loc.length === 1 && toc[loc[0]].chapters) {
         // @ts-ignore
@@ -363,18 +362,18 @@ const fixLocation = (href: string | undefined, loc: number[] | undefined) => {
 
     if (loc.length === 2 && toc[loc[0]].chapters) {
         // @ts-ignore
-        var currChapter = toc[loc[0]].chapters[loc[1]]
+        const currChapter = toc[loc[0]].chapters[loc[1]]
 
         if (currChapter.sections && currChapter.sections.length > 0) {
-            const sectionDiv = href.split('#')[1]
+            const sectionDiv = href.split("#")[1]
             if (sectionDiv === undefined) {
                 // when opening a chapter-level page, also present the first section
                 loc.push(0) // add the first section (index 0)
                 href = currChapter.sections[0].URL
             } else {
-                //section id was sent in href, present the corresponding section
+                // section id was sent in href, present the corresponding section
                 for (let i = 0; i < currChapter.sections.length; i++) {
-                    if (sectionDiv === currChapter.sections[i].URL.split('#')[1]) {
+                    if (sectionDiv === currChapter.sections[i].URL.split("#")[1]) {
                         loc.push(i)
                         href = currChapter.sections[i].URL
                         break
@@ -386,6 +385,6 @@ const fixLocation = (href: string | undefined, loc: number[] | undefined) => {
         }
     }
 
-    const curriculumDir = '../curriculum/'
-    return {href: curriculumDir + href, loc}
+    const curriculumDir = "../curriculum/"
+    return { href: curriculumDir + href, loc }
 }
