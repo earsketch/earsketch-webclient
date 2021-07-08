f7587378dc0fd34db967ab61f30df9f7ec730de3
Ian Clester <ijc@ijc8.me>
2021-07-08 09:40:41 -0400

    Show owner for un-imported shared scripts.

--- scripts/src/browser/Scripts.tsx
+++ scripts/src/browser/Scripts.tsx
@@ -364,12 +364,8 @@ const Script: React.FC<ScriptProps> = ({ script, bgTint, type }) => {
                             {script.name}
                         </div>
                         <div className='pr-4 space-x-2'>
-                            {
-                                (shared && !script.collaborative) && (<i className='icon-copy3 align-middle' title={`Shared by ${script.creator}`} />)
-                            }
-                            {
-                                script.collaborative && (<i className='icon-users4 align-middle' title={`Shared with ${collaborators.join(', ')}`} />)
-                            }
+                            {(shared && !script.collaborative) && (<i className='icon-copy3 align-middle' title={`Shared by ${script.creator ?? script.username}`} />)}
+                            {script.collaborative && (<i className='icon-users4 align-middle' title={`Shared with ${collaborators.join(', ')}`} />)}
                         </div>
                     </div>
                     <div className={`${type==='regular' ? 'flex' : 'hidden'} flex-column items-center space-x-4`}>
