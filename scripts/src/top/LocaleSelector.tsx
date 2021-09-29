2a564676c3b6d622ea0dacacf4fcf021d9c8641a
Stephen Garrett <manodrum@gmail.com>
2021-09-29 14:58:14 -0400

    add french locale

--- scripts/src/top/LocaleSelector.tsx
+++ scripts/src/top/LocaleSelector.tsx
@@ -15,6 +15,7 @@ interface locale {
 export const AVAILABLE_LOCALES: locale[] = [
     { displayText: "English", localeCode: "en" },
     { displayText: "Español", localeCode: "es" },
+    { displayText: "Française", localeCode: "fr" },
 ]
 
 export const LocaleSelector = () => {
