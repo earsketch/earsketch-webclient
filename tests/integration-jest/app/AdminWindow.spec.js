697dcbbce93155861a087c8afd09298e06c45e56
heerman <lwheerman@gmail.com>
2021-11-03 12:36:58 -0400

    admin window test failing on import

---
+++ tests/integration-jest/app/AdminWindow.spec.js
@@ -0,0 +1,6 @@
+import { AdminWindow } from "../../../scripts/src/app/AdminWindow"
+
+describe('Test suite for AdminWindow', function () {
+    it("Test of admin window... todo", function () {})
+        expect(2 + 2).toBe(4)
+})
