ccf9578b5f066410312559fdd72117f7eee8cea0
heerman <lwheerman@gmail.com>
2021-12-09 14:58:11 -0500

    adminwindow component rendered for test

--- tests/integration-jest/app/AdminWindow.spec.js
+++ tests/integration-jest/app/AdminWindow.spec.js
@@ -1,9 +1,11 @@
 /* eslint-env jest */
+import React from "react"
+import renderer from "react-test-renderer"
 import "../mocks/AudioContext.mock"
-// import { AdminWindow } from "../../../scripts/src/app/AdminWindow"
+import { AdminWindow } from "../../../scripts/src/app/AdminWindow"
 
-describe("Test suite for AdminWindow", () => {
-    console.log("test here")
-    it("Test of admin window... todo", () => {})
-    expect(2 + 2).toBe(4)
+describe("AdminWindow", () => {
+    it("renders", () => {})
+    const component = renderer.create(<AdminWindow></AdminWindow>)
+    console.log("admin window component=" + JSON.stringify(component))
 })
