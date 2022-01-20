c53566ab1002f999792fb0003caeb83b0761fbe2
Erin Truesdell <nern.strudel@gmail.com>
2022-01-20 12:28:55 -0500

    eh updates/additions

--- scripts/src/cai/errorHandling.ts
+++ scripts/src/cai/errorHandling.ts
@@ -61,6 +61,7 @@ export function handleError(errorType: string) {
     // get line of error
     textArray = currentText.split("\n")
     errorLine = textArray[currentError.traceback[0].lineno - 1]
+    basicChecks()
 
     // check first for undefined variables
     if (errorType === "NameError") {
@@ -146,6 +147,12 @@ export function handleError(errorType: string) {
             return handleConditionalError()
         }
     }
+}
+
+function basicChecks() {
+    // check for import, init, and finish
+    
+    
 }
 
 function handleFunctionError() {
@@ -494,37 +501,52 @@ function handleWhileLoopError() {
 }
 
 function handleConditionalError() {
-    if (!errorLine.includes("(") || !errorLine.includes(")")) {
-        return ["conditional", "missing parentheses"]
-    }
-    // are there matching numbers of parens
-    const openParens = (errorLine.split("(").length - 1)
-    const closeParens = (errorLine.split(")").length - 1)
+    if (errorLine.includes("if")) { // if or elif
+        if (!errorLine.includes("(") || !errorLine.includes(")")) {
+            return ["conditional", "missing parentheses"]
+        }
+        // are there matching numbers of parens
+        const openParens = (errorLine.split("(").length - 1)
+        const closeParens = (errorLine.split(")").length - 1)
 
-    if (openParens !== closeParens) {
-        return ["conditional", "parentheses mismatch"]
-    }
+        if (openParens !== closeParens) {
+            return ["conditional", "parentheses mismatch"]
+        }
 
-    // again, right now we'll ignore the condition. but let's amke sure there's a colon
-    if (!ccHelpers.trimCommentsAndWhitespace(errorLine).endsWith(":")) {
-        return ["conditional", "missing colon"]
-    }
+        // again, right now we'll ignore the condition. but let's amke sure there's a colon
+        if (!ccHelpers.trimCommentsAndWhitespace(errorLine).endsWith(":")) {
+            return ["conditional", "missing colon"]
+        }
 
-    // check for body
-    // find next non-blank line (if there is one). assess indent
-    let nextLine: string = ""
-    for (let i = currentError.traceback[0].lineno; i < textArray.length; i++) {
-        nextLine = textArray[i]
-        if (nextLine !== "") {
-            break
+        // check for body
+        // find next non-blank line (if there is one). assess indent
+        let nextLine: string = ""
+        for (let i = currentError.traceback[0].lineno; i < textArray.length; i++) {
+            nextLine = textArray[i]
+            if (nextLine !== "") {
+                break
+            }
+        }
+        // compare indent on nextLine vs errorLine
+        if (ccHelpers.numberOfLeadingSpaces(nextLine) <= ccHelpers.numberOfLeadingSpaces(errorLine)) {
+            return ["conditional", "missing body"]
         }
     }
-    // compare indent on nextLine vs errorLine
-    if (ccHelpers.numberOfLeadingSpaces(nextLine) <= ccHelpers.numberOfLeadingSpaces(errorLine)) {
-        return ["conditional", "missing body"]
-    }
 
-    // now, the fun parts: looking for a misindented else
+    // looking for a misindented else
+
+    if (errorLine.includes("elif") || errorLine.includes("else")) {
+        // we have to look upwards in the code for this. if the next unindented line about this on ISN'T an if or an elif, we have a problem.
+        let nextLineUp: string = ""
+        for (let i = currentError.traceback[0].lineno; i > 0; i--) {
+            nextLineUp = textArray[i]
+            if (nextLineUp !== "" && ccHelpers.numberOfLeadingSpaces(nextLineUp) <= ccHelpers.numberOfLeadingSpaces(errorLine)) {
+                if (!nextLineUp.includes("if")) {
+                    return ["conditional", "misindented else"]
+                }
+            }
+        }
+    }
 }
 
 function handleNameError() {
