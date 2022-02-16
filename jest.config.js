1c09be13a3c9d1755bc7a79afddd20562960411d
Stephen Garrett <manodrum@gmail.com>
2022-02-16 13:44:20 -0500

    remove commented code and unused requires

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-const { pathsToModuleNameMapper } = require("ts-jest")
-// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
-// which contains the path mapping (ie the `compilerOptions.paths` option):
-const { compilerOptions } = require("./tsconfig")
 
 module.exports = {
     preset: "ts-jest/presets/js-with-babel",
@@ -24,21 +20,10 @@ module.exports = {
     moduleNameMapper: {
         "earsketch-dsp": "<rootDir>/scripts/lib/earsketch-dsp.d.ts",
         recorder: "<rootDir>/scripts/lib/recorderjs/recorder.js",
-        // ...pathsToModuleNameMapper(compilerOptions.paths /*, { prefix: '<rootDir>/' } */),
-        // "/^(.*)$/": "<rootDir>/scripts/src/types/$1.d.ts",
-        // "^dsp(.*)$": "<rootDir>/scripts/src/types/global.d.ts",
-        // "earsketch-dsp(.*)$": "<rootDir>/scripts/lib/earsketch-dsp.js",
-        // // mock earsketch-dsp?
         ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
         // Resolve .jpg and similar files to __mocks__/file-mock.js
         ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/file-mock.js",
     },
     testPathIgnorePatterns: ["node_modules", "\\.cache"],
     moduleDirectories: ["node_modules", "scripts/lib"],
-    // moduleFileExtensions: [
-    //     "ts",
-    //     "tsx",
-    //     "js",
-    //     "jsx",
-    // ],
 }
