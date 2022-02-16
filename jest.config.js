fa35a8cc7f5230e6b11390dca7fd5252f618ef56
heerman <lwheerman@gmail.com>
2022-02-16 11:13:25 -0500

    replace occurrences of ts-jest/utils with ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-const { pathsToModuleNameMapper } = require("ts-jest/utils")
+const { pathsToModuleNameMapper } = require("ts-jest")
 // In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
 // which contains the path mapping (ie the `compilerOptions.paths` option):
 const { compilerOptions } = require("./tsconfig")
