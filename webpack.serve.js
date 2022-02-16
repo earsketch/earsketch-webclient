de50a7537c9bc6cc043eff8c92cae9cf794dc6be
Stephen Garrett <manodrum@gmail.com>
2022-02-16 18:31:14 -0500

    updated all dependencies, webpack builds
    but there are css / font / other asset issues

--- webpack.serve.js
+++ webpack.serve.js
@@ -26,9 +26,11 @@ module.exports = env => {
             publicPath: "/", // webclient folder
         },
         devServer: {
-            publicPath: "/",
             port: port,
-            hotOnly: true,
+            hot: "only",
+            devMiddleware: {
+                publicPath: "/",
+            },
         },
         module: {
             rules: [{
