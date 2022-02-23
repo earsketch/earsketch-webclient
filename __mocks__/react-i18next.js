aa55ec0b0c70af8665325c537fe42119d7ea1a63
heerman <lwheerman@gmail.com>
2022-02-23 12:00:33 -0500

    browser test

---
+++ __mocks__/react-i18next.js
@@ -0,0 +1,1 @@
+export const useTranslation = function () { return "TRANSLATED_TEXT" }
---
+++ tests/integration-jest/src/browser/Browser.spec.js
@@ -0,0 +1,14 @@
+/* eslint-env jest */
+import React from "react"
+import "../../AudioContextMock/AudioContext.mock" // jsdom is missing AudioContext, so we provide it
+import { render, screen } from "@testing-library/react"
+import { Browser } from "../../../../scripts/src/browser/Browser" // called by our test below
+import store, { persistor } from "../../../../scripts/src/reducers"
+import { Provider } from "react-redux"
+
+import { useTranslation } from "react-i18next"
+jest.mock("react-i18next")
+
+it("renders with mocked data", async () => {
+  render(<Provider store={store}><Browser /></Provider>)
+})
