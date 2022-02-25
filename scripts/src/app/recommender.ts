d5c991b16b7fe8695a35ad6f15dc23e6f6004523
Ashvala Vinay <mail@ashvala.net>
2022-02-25 14:04:40 -0500

    fix some linter issues

--- scripts/src/app/recommender.ts
+++ scripts/src/app/recommender.ts
@@ -147,23 +147,27 @@ export function recommendReverse(recommendedSounds: string[], inputSamples: stri
     return filteredRecs
 }
 
-function generateRecommendations(inputSamples: string[], coUsage: number = 1, similarity: number = 1) {
+function generateRecommendations(inputSamples: string[], coUsage: number = 1, similarity: number = 1, keyStrictness: boolean = true) {
     // Co-usage and similarity for alternate recommendation types: 1 - maximize, -1 - minimize, 0 - ignore.
     coUsage = Math.sign(coUsage)
     similarity = Math.sign(similarity)
+    const estimatedKey = estimateKeySignature(inputSamples)
+    console.log(`current estimated Key: ${KEY_LABELS[estimatedKey]}`)
     // Generate recommendations for each input sample and add together
     const recs: { [key: string]: number } = Object.create(null)
-    // console.log(inputSamples)
+
     for (const inputSample of inputSamples) {
         const audioNumber = Object.keys(NUMBERS_AUDIOKEYS).find(n => NUMBERS_AUDIOKEYS[n] === inputSample)
         if (audioNumber !== undefined) {
             const audioRec = AUDIOKEYS_RECOMMENDATIONS[audioNumber]
             for (const [num, value] of Object.entries(audioRec)) {
                 const soundObj = NUMBERS_AUDIOKEYS[`${num}`]
-                const fullVal = value[0] + coUsage * value[1] + similarity * value[2]
-
+                // console.log(`soundObj: ${soundObj}, keySignature: ${KEYSIGNATURES_STRING[soundObj].keysig}`)
+                const match = KEYSIGNATURES_STRING[soundObj].keysig === estimatedKey
+                const keyScore = (keyStrictness ? (match ? 1 : 0) : 0) * 4
+                console.log(`The keyscore is: ${keyScore}`)
+                const fullVal = value[0] + coUsage * value[1] + similarity * value[2] + keyScore
                 const key = NUMBERS_AUDIOKEYS[num]
-
                 if (key in recs) {
                     recs[key] = (fullVal + recs[key]) / 1.41
                 } else {
@@ -176,11 +180,10 @@ function generateRecommendations(inputSamples: string[], coUsage: number = 1, si
 }
 
 function filterRecommendations(inputRecs: { [key: string]: number }, recommendedSounds: string[], inputSamples: string[],
-    genreLimit: string[], instrumentLimit: string[], previousRecommendations: string[], bestLimit: number) {
+    genreLimit: string[], instrumentLimit: string[], previousRecommendations: string[], bestLimit: number, keyStrictness: boolean = true) {
     const recs: { [key: string]: number } = {}
-    console.log(inputSamples)
-    const estimated_key = estimateKeySignature(inputSamples)
-    console.log(estimated_key)
+    const recommendedSignatures = recommendedSounds.map(s => KEY_LABELS[estimateKeySignature([s])])
+    console.log("the key signatures in the recommended sounds are: ", recommendedSignatures)
     for (const key in inputRecs) {
         if (!recommendedSounds.includes(key) && !inputSamples.includes(key) &&
             !previousRecommendations.includes(key) && key.slice(0, 3) !== "OS_") {
@@ -242,12 +245,12 @@ export function availableInstruments() {
 export function getKeySignatureString(filename: string) {
     // For a given filename, return the key signature of the file using the KEYSIGNATURES object.
     // If the file is not in the database, return "N/A"
-    const audioNumber = AUDIOKEYS.indexOf(filename)
-
-    if (audioNumber === -1) {
-        return "N/A"
-    }
-    const keyClass = KEYSIGNATURES[`${audioNumber}`]
+    // const audioNumber = KEYSIGNATURES_STRING
+    // if (audioNumber === -1) {
+    //     return "N/A"
+    // }
+    const keyClass = KEYSIGNATURES_STRING[`${filename}`].keysig
+    console.log(keyClass)
     if (keyClass !== -1) {
         const keyLabel = KEY_LABELS[keyClass]
         return keyLabel
