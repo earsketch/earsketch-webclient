26badb6490671a5ce1efd372a0c70ddab9172997
Stephen Garrett <manodrum@gmail.com>
2022-03-01 14:53:14 -0500

    jest script test in progress

--- tests/integration-jest/src/scripts/curriculum.spec.js
+++ tests/integration-jest/src/scripts/curriculum.spec.js
@@ -1,9 +1,16 @@
 /* eslint-env jest */
 import "../../AudioContextMock/AudioContext.mock" // jsdom is missing AudioContext, so we provide it
+import fetchMock from "jest-fetch-mock"
 
 import * as ESUtils from "../../../../scripts/src/esutils"
 import * as runner from "../../../../scripts/src/app/runner"
 import { CURRICULUM_RESULTS } from "../../../integration/scripts/curriculum.results"
+
+fetchMock.enableMocks()
+
+beforeEach(() => {
+    fetch.resetMocks()
+})
 
 const script = "var tempo = \"99\";\n" +
   "tempo = Number(tempo);\n" +
