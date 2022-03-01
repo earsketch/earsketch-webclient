d17ad6d6e70761fa99f5392d10d732cc28be2697
heerman <lwheerman@gmail.com>
2022-03-01 16:15:11 -0500

    linter def never used

--- tests/integration-jest/src/browser/Browser.spec.js
+++ tests/integration-jest/src/browser/Browser.spec.js
@@ -3,7 +3,7 @@ import React from "react"
 import "../../AudioContextMock/AudioContext.mock" // jsdom is missing AudioContext, so we provide it
 import { render, screen } from "@testing-library/react"
 import { Browser } from "../../../../scripts/src/browser/Browser" // called by our test below
-import store, { persistor } from "../../../../scripts/src/reducers"
+import store from "../../../../scripts/src/reducers"
 import { Provider } from "react-redux"
 
 jest.mock("react-i18next")
