363ffdb0ba7a3e97ff8751b1556cfb70c1d923c9
Stephen Garrett <manodrum@gmail.com>
2022-03-02 08:44:15 -0500

    remove fetchMock

--- tests/integration-jest/src/scripts/curriculum.spec.js
+++ tests/integration-jest/src/scripts/curriculum.spec.js
@@ -1,15 +1,13 @@
 /* eslint-env jest */
 import "../../AudioContextMock/AudioContext.mock" // jsdom is missing AudioContext, so we provide it
-import fetchMock from "jest-fetch-mock"
 
 import * as runner from "../../../../scripts/src/app/runner"
 
-fetchMock.enableMocks()
 jest.mock("../../../../scripts/src/app/audiolibrary")
 
-beforeEach(() => {
-    fetch.resetMocks()
-})
+// beforeEach(() => {
+//     fetch.resetMocks()
+// })
 
 const script = "var tempo = \"99\";\n" +
   "tempo = Number(tempo);\n" +
