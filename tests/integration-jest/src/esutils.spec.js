fcc0471b19e8099982ca50eee61df226ddef444d
Ashvala Vinay <mail@ashvala.net>
2022-03-03 22:54:01 -0500

    Create unit tests for compareObjStructure

--- tests/integration-jest/src/esutils.spec.js
+++ tests/integration-jest/src/esutils.spec.js
@@ -1,5 +1,6 @@
 /* eslint-env jest */
 import { parseLanguage } from "../../../scripts/src/esutils"
+import { compareObjStructure } from "../../../scripts/src/esutils"
 
 it("detects py with parseLanguage()", () => {
     expect(parseLanguage("song.py")).toBe("python")
@@ -7,4 +8,38 @@ it("detects py with parseLanguage()", () => {
 
 it("detects js with parseLanguage()", () => {
     expect(parseLanguage("song.js")).toBe("javascript")
+})
+
+it("returns true when two empty objects are passed in", () => {
+    expect(compareObjStructure({}, {})).toBe(true)
+})
+
+it("compareObjStructure returns true when two fully identical JSON objects are passed in", () => {
+    const structA = { a: 1, b: 2, c: 3 }
+    const structB = { a: 1, b: 2, c: 3 }
+    expect(compareObjStructure(structA, structB)).toBe(true)
+})
+
+it("compareObjStructure returns false when two dissimilar JSON objects are passed in", () => {
+    const structA = { a: 1, b: 2, c: 3 }
+    const structB = { a: 1, b: 2 }
+    expect(compareObjStructure(structA, structB)).toBe(false)
+})
+
+it("compareObjStructure returns true when two identical JSON structures are passed in with different values", () => {
+    const structA = { a: 1, b: 2, c: 3 }
+    const structB = { a: 1, b: 2, c: 4 }
+    expect(compareObjStructure(structA, structB)).toBe(true)
+})
+
+it("compareObjStructure returns true when two deep JSON structures are passed in", () => {
+    const structA = { a: 1, b: 2, c: { d: 3, e: 4 } }
+    const structB = { a: 1, b: 2, c: { d: 3, e: 4 } }
+    expect(compareObjStructure(structA, structB)).toBe(true)
+})
+
+it("compareObjStructure returns false when two deep, but dissimilar JSON objects are passed in", () => {
+    const structA = { a: 1, b: 2, c: { d: 3, e: 4 } }
+    const structB = { a: 1, b: 2, c: { d: 3 } }
+    expect(compareObjStructure(structA, structB)).toBe(false)
 })
