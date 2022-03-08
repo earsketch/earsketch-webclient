b8040753f10f96664c0eb07bd7398cc247a84e63
heerman <lwheerman@gmail.com>
2022-03-08 16:49:50 -0500

    render sound browser and click it

--- tests/integration-jest/src/browser/Browser.spec.js
+++ tests/integration-jest/src/browser/Browser.spec.js
@@ -1,15 +1,19 @@
 /* eslint-env jest */
 import React from "react"
-import "../../AudioContextMock/AudioContext.mock" // jsdom is missing AudioContext, so we provide it
-import { render, screen } from "@testing-library/react"
-import { Browser } from "../../../../scripts/src/browser/Browser" // called by our test below
-import store from "../../../../scripts/src/reducers"
-import { Provider } from "react-redux"
+import { render } from "@testing-library/react" // component rendering
+import { screen } from "@testing-library/dom" // find elements on screen
+import "@testing-library/jest-dom" // assertions
+import userEvent from "@testing-library/user-event" // clicking
+import "../../AudioContextMock/AudioContext.mock" // jsdom has no AudioContext
+import { Provider } from "react-redux" // redux
+import store from "../../../../scripts/src/reducers" // earsketch redux store
+import { Browser } from "../../../../scripts/src/browser/Browser"
 
 jest.mock("react-i18next")
 jest.mock("../../../../scripts/src/app/audiolibrary")
 
 it("renders with mocked data", async () => {
     render(<Provider store={store}><Browser /></Provider>)
-    await screen.findByText("soundBrowser.filterDropdown.instruments")
+    await screen.findByText("soundBrowser.button.addSound")
+    userEvent.click(screen.getByText("soundBrowser.button.addSound"))
 })
