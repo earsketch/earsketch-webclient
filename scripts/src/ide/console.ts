bcb06b16292156718c93684aa986954f1e84b0f9
Stephen Garrett <manodrum@gmail.com>
2022-03-28 12:20:44 -0400

    localize custom console messages

--- scripts/src/ide/console.ts
+++ scripts/src/ide/console.ts
@@ -1,3 +1,4 @@
+import i18n from "i18next"
 import * as ide from "./ideState"
 import store from "../reducers"
 
@@ -15,11 +16,11 @@ export function log(text: string) {
 }
 
 export function warn(text: string) {
-    store.dispatch(ide.pushLog({ level: "warn", text: "Warning message >> " + text }))
+    store.dispatch(ide.pushLog({ level: "warn", text: i18n.t("console:warningHeading") + " >> " + text }))
 }
 
 export function error(error: string | Error) {
-    store.dispatch(ide.pushLog({ level: "error", text: "Error message >> " + elaborate(error) }))
+    store.dispatch(ide.pushLog({ level: "error", text: i18n.t("console:errorHeading") + " >> " + elaborate(error) }))
 }
 
 // Elaborate error messages printed to the user console. Available cases based on Skulpt's error messages and Node.js's errors.
@@ -29,59 +30,59 @@ export function elaborate(error: string | Error) {
     switch (parts[0]) {
         // Generic & Python-specific errors from Skulpt (errors.js)
         case "AssertionError":
-            parts[0] = "AssertionError: An assert statement failed"
+            parts[0] = i18n.t("console:errors.AssertionError")
             break
         case "AttributeError":
-            parts[0] = "AttributeError: There is a mismatch between the object and the attribute"
+            parts[0] = i18n.t("console:errors.AttributeError")
             break
         case "ImportError":
-            parts[0] = "ImportError: The appropriate packages cannot be found or imported"
+            parts[0] = i18n.t("console:errors.ImportError")
             break
         case "IndentationError":
-            parts[0] = "IndentationError: There is an indentation error in the code (lack or extra spaces)"
+            parts[0] = i18n.t("console:errors.IndentationError")
             break
         case "IndexError":
-            parts[0] = "IndexError: There is an error using an out of range index"
+            parts[0] = i18n.t("console:errors.IndexError")
             break
         case "KeyError":
-            parts[0] = "KeyError: There is an error using a dictionary key that does not exist"
+            parts[0] = i18n.t("console:errors.KeyError")
             break
         case "NameError":
-            parts[0] = "NameError: There is an error with a variable or function name that is not defined"
+            parts[0] = i18n.t("console:errors.NameError")
             break
         case "ParseError":
-            parts[0] = "ParseError: There is an error when reading the code"
+            parts[0] = i18n.t("console:errors.ParseError")
             break
         case "SyntaxError":
-            parts[0] = "SyntaxError: There is an error with the syntax (or arrangement) of code"
+            parts[0] = i18n.t("console:errors.SyntaxError")
             break
         case "TypeError":
-            parts[0] = "TypeError: There is an error with the expected data type"
+            parts[0] = i18n.t("console:errors.TypeError")
             break
         case "TokenError":
-            parts[0] = "TokenError: There is an unexpected token error (extra or missing comma, space, or character) in the code"
+            parts[0] = i18n.t("console:errors.TokenError")
             break
         case "ValueError":
-            parts[0] = "ValueError: A provided argument is not within the set or range of acceptable values for a function"
+            parts[0] = i18n.t("console:errors.ValueError")
             break
         // JS-specific errors
         case "RangeError":
-            parts[0] = "RangeError: A provided argument is not within the set or range of acceptable values for a function"
+            parts[0] = i18n.t("console:errors.RangeError")
             break
         case "ReferenceError":
-            parts[0] = "ReferenceError: There is an error with a variable or function name that is not defined"
+            parts[0] = i18n.t("console:errors.ReferenceError")
             break
         case "Unknown identifier":
-            parts[0] = "ReferenceError: There is an error with a variable or function name that is not defined"
+            parts[0] = i18n.t("console:errors.UnknownIdentifier")
             break
         // HTTP errors while communicating with server
         case "NetworkError":
             parts[0] = msg
-            parts[1] = " please try running the code again. If the issue persists, check your internet connection or contact network administrator."
+            parts[1] = " " + i18n.t("console:errors.NetworkError")
             break
         case "ServerError":
             parts[0] = msg
-            parts[1] = " please try running the code again. If the issue persists, please report the issue using 'Report Error' in the options menu."
+            parts[1] = " " + i18n.t("console:errors.ServerError")
             break
         default:
             return msg
