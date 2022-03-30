759d699f230c64009c278a5645d13c9edf2e1956
Ashvala Vinay <mail@ashvala.net>
2022-03-30 19:41:17 -0400

    cypress test for Table of Contents

---
+++ tests/e2e-cypress/integration/tableofcontents.spec.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+describe("Table of content", () => {
+    beforeEach(() => {
+        cy.intercept(
+            {
+                method: "GET",
+                hostname: "api-dev.ersktch.gatech.edu",
+                path: "/EarSketchWS/audio/standard",
+            },
+            {
+                body: [],
+            }
+        )
+        cy.visit("http://localhost:8888")
+    })
+
+    it("shows TOC", () => {
+        cy.get("button").contains("Skip").click()
+        cy.get("button").contains("Welcome Students and Teachers!").click()
+    })
+
+    it("opens a chapter", () => {
+        cy.get("button").contains("Skip").click()
+        cy.get("button").contains("Welcome Students and Teachers!").click()
+        cy.get("a").contains("Unit 1: Compose and Add Beats").parent(".flex .items-start").get("button[title='Expand ']").first().click()
+        cy.get("a").contains("Get Started with EarSketch").click()
+    })
+})
