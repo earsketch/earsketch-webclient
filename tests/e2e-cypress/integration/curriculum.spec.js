0c3a406d215a4dea92830f887000823e951b1d03
Ashvala Vinay <mail@ashvala.net>
2022-04-13 14:22:56 -0400

    clean up the test code for opening up a chapter

--- tests/e2e-cypress/integration/curriculum.spec.js
+++ tests/e2e-cypress/integration/curriculum.spec.js
@@ -3,17 +3,16 @@ describe("Curriculum", () => {
     beforeEach(() => {
         cy.interceptAudioStandard()
         cy.visit("/")
+        cy.get("button").contains("Skip").click()
     })
 
     it("shows TOC", () => {
-        cy.get("button").contains("Skip").click()
         cy.get("button").contains("Welcome Students and Teachers!").click()
     })
 
     it("opens a chapter", () => {
-        cy.get("button").contains("Skip").click()
         cy.get("button").contains("Welcome Students and Teachers!").click()
-        cy.get("a").contains("Unit 1: Compose and Add Beats").parent(".flex .items-start").get("button[title='Expand ']").first().click()
-        cy.get("a").contains("Get Started with EarSketch").click()
+        cy.get("button[title='Expand ']").first().click()
+        cy.contains("a", "Get Started with EarSketch").click()
     })
 })
