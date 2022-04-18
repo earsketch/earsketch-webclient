c3d3b714eb9a3847ef9d834041c42458039d460c
heerman <lwheerman@gmail.com>
2022-04-18 10:30:43 -0400

    run code received during collab test

--- tests/e2e-cypress/integration/collaboration.spec.js
+++ tests/e2e-cypress/integration/collaboration.spec.js
@@ -6,6 +6,9 @@ describe("collaboration", () => {
     const wsServer = createServer("wss://api-dev.ersktch.gatech.edu/EarSketchWS/socket/cypress/")
 
     it("joins and edits", () => {
+        const scriptText = "#\t\tpython code\n#\t\tscript_name:\n#\n#\t\tauthor:\n#\t\tdescription:\n#\n\nfrom earsketch import *\n\ninit()\nsetTempo(120)\n\n\n\nfinish()\n"
+        const scriptShareId = "-WNilN4_g8r3TkUZpHaymw"
+
         const myCollabScripts = [
             {
                 name: "live_code_with_me.py",
@@ -19,11 +22,48 @@ describe("collaboration", () => {
                 license_id: 1,
                 modified: "2022-02-03 19:04:38.0",
                 run_status: 2,
-                shareid: "-WNilN4_g8r3TkUZpHaymw",
-                source_code: "#\t\tpython code\n#\t\tscript_name:\n#\n#\t\tauthor:\n#\t\tdescription:\n#\n\nfrom earsketch import *\n\ninit()\nsetTempo(120)\n\n\n\nfinish()\n",
+                shareid: scriptShareId,
+                source_code: scriptText,
                 username: "friend2",
             },
         ]
+
+        const receiveCollabCode = (code) => {
+            // receive incoming collaboration typing via websocket
+            let randId = 1
+            let cursorPos = 110
+            const receiveCollabChar = (nextChar) => {
+                cy.incomingWebSocketMessage(
+                    wsServer,
+                    {
+                        action: "cursorPosition",
+                        notification_type: "collaboration",
+                        position: cursorPos,
+                        scriptID: scriptShareId,
+                        sender: "friend2",
+                        state: 0,
+                    }
+                )
+                cy.incomingWebSocketMessage(
+                    wsServer,
+                    {
+                        ID: randId.toString(),
+                        action: "edit",
+                        editData: { user: "friend2", action: "insert", start: cursorPos, end: cursorPos + 1, len: 1, text: nextChar },
+                        notification_type: "collaboration",
+                        scriptID: scriptShareId,
+                        sender: "friend2",
+                        state: 0,
+                    }
+                )
+                randId += 1
+                cursorPos += 1
+            }
+            // send each character of the message one at a time, like the client
+            for (let i = 0; i < code.length; i++) {
+                receiveCollabChar(code.charAt(i))
+            }
+        }
 
         cy.interceptAudioStandard()
         cy.interceptUsersToken()
@@ -36,109 +76,42 @@ describe("collaboration", () => {
         cy.visitWithStubWebSocket("/", MockSocket.WebSocket)
         cy.login()
 
+        // open a script with collaboration
         cy.get("button[title='Open SCRIPTS Tab']").click()
         cy.contains("div", "SHARED SCRIPTS (1)").click()
         cy.contains("div", "live_code_with_me.py").click()
 
+        // join a script collaboration session
         cy.incomingWebSocketMessage(
             wsServer,
             {
                 action: "joinedSession",
                 activeMembers: ["cypress"],
                 notification_type: "collaboration",
-                scriptID: "-WNilN4_g8r3TkUZpHaymw",
-                scriptText: "#\t\tpython code\n#\t\tscript_name:\n#\n#\t\tauthor:\n#\t\tdescription:\n#\n\nfrom earsketch import *\n\ninit()\nsetTempo(120)\n\n\n\nfinish()\n",
+                scriptID: scriptShareId,
+                scriptText: scriptText,
                 sender: "cypress",
                 state: 0,
             }
         )
 
+        // collaborator now joins our session
         cy.incomingWebSocketMessage(
             wsServer,
             {
                 action: "memberJoinedSession",
                 notification_type: "collaboration",
-                scriptID: "-WNilN4_g8r3TkUZpHaymw",
+                scriptID: scriptShareId,
                 sender: "friend2",
             }
         )
 
-        cy.incomingWebSocketMessage(
-            wsServer,
-            {
-                action: "cursorPosition",
-                notification_type: "collaboration",
-                position: 110,
-                scriptID: "-WNilN4_g8r3TkUZpHaymw",
-                sender: "friend2",
-                state: 0,
-            }
-        )
+        // receive incoming collaboration typing to our script
+        const code = `print("mU" + "${51}" + "k!")`
+        receiveCollabCode(code)
 
-        cy.incomingWebSocketMessage(
-            wsServer,
-            {
-                ID: "q847zzxcpf",
-                action: "edit",
-                editData: { len: 1, start: 110, action: "insert", end: 111, text: "#", user: "friend2" },
-                notification_type: "collaboration",
-                scriptID: "-WNilN4_g8r3TkUZpHaymw",
-                sender: "friend2",
-                state: 0,
-            }
-        )
-
-        cy.incomingWebSocketMessage(
-            wsServer,
-            {
-                action: "cursorPosition",
-                notification_type: "collaboration",
-                position: 111,
-                scriptID: "-WNilN4_g8r3TkUZpHaymw",
-                sender: "friend2",
-                state: 1,
-            }
-        )
-
-        cy.incomingWebSocketMessage(
-            wsServer,
-            {
-                ID: "xkv67uiei7",
-                action: "edit",
-                editData: { len: 1, start: 111, action: "insert", end: 112, text: "h", user: "friend2" },
-                notification_type: "collaboration",
-                scriptID: "-WNilN4_g8r3TkUZpHaymw",
-                sender: "friend2",
-                state: 1,
-            }
-        )
-
-        cy.incomingWebSocketMessage(
-            wsServer,
-            {
-                action: "cursorPosition",
-                notification_type: "collaboration",
-                position: 112,
-                scriptID: "-WNilN4_g8r3TkUZpHaymw",
-                sender: "friend2",
-                state: 2,
-            }
-        )
-
-        cy.incomingWebSocketMessage(
-            wsServer,
-            {
-                ID: "f3y31uslub",
-                action: "edit",
-                editData: { len: 1, start: 112, action: "insert", end: 113, text: "i", user: "friend2" },
-                notification_type: "collaboration",
-                scriptID: "-WNilN4_g8r3TkUZpHaymw",
-                sender: "friend2",
-                state: 2,
-            }
-        )
-
-        // verify received text appears in editor
-        // cy.contains("#hi") // currently fails
+        // run the script and verify that we received the code
+        cy.get('#run-button').click()
+        cy.contains("mU51k!")
     })
 })
