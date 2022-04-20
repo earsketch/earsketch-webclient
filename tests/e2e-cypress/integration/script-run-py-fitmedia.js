c6cf97a31edb93bcbfc267e521bfaa42978083a6
Sean Tillman <sean.tillman18@gmail.com>
2022-04-20 11:15:03 -0400

    Modified to loading script data rather than creating new file

--- tests/e2e-cypress/integration/script-run-py-fitmedia.js
+++ tests/e2e-cypress/integration/script-run-py-fitmedia.js
@@ -3,44 +3,58 @@ import * as MockSocket from "mock-socket"
 
 describe("fitMedia (py) script", () => {
     it("Logs in, creates a new script, runs with fit media", () => {
-        const cypTestScriptId = Math.floor(Math.random() * 1000)
-        const cypTestName = "test" + cypTestScriptId
+        // Stubbing
+        const testSoundMeta = {
+            artist: "RICHARD DEVINE",
+            folder: "DUBSTEP_140_BPM__DUBBASSWOBBLE",
+            genre: "DUBSTEP",
+            genreGroup: "DUBSTEP",
+            instrument: "SYNTH",
+            name: "DUBSTEP_BASS_WOBBLE_002",
+            path: "filename/placeholder/here.wav",
+            public: 1,
+            tempo: 140,
+            year: 2012,
+        }
+        const scriptData = {
+            created: "2021-10-12 20:17:18.0",
+            file_location: "",
+            id: -1,
+            modified: "2021-10-12 20:22:29.0",
+            name: "playsound.py",
+            run_status: 1,
+            shareid: "qeT7pez_OVHwmxeDVzkT7w",
+            soft_delete: false,
+            source_code: "from earsketch import *\n\ninit()\nsetTempo(120)\nfitMedia(DUBSTEP_BASS_WOBBLE_002, 1, 1, 5)\n\nfinish()\n",
+            username: "cypress",
+        }
 
-        // Standard Stubbing
-        cy.interceptAudioStandard()
+        cy.interceptAudioStandard([testSoundMeta])
+        cy.interceptAudioMetadata(testSoundMeta)
+        cy.interceptAudioSample()
+
         cy.interceptUsersToken()
         cy.interceptUsersInfo()
         cy.interceptAudioUser()
         cy.interceptAudioFavorites()
-        cy.interceptScriptsOwned()
+
+        cy.interceptScriptsOwned([scriptData])
         cy.interceptScriptsShared()
-        cy.interceptScriptSave(cypTestName + ".py")
+        cy.interceptScriptSave("playsound.py", scriptData)
 
         cy.visitWithStubWebSocket("/", MockSocket.WebSocket)
         cy.login()
 
         cy.get("#app-title").should("contain", "EarSketch")
 
-        // Add Script
+        // Load Script
         cy.get("button[title='Open SCRIPTS Tab']").click()
-        cy.get("button[title='Create a new script']").click()
-        cy.get("input[name='Script Name']").type(cypTestName)
-        cy.get("input[value='CREATE']").click()
-
-        // Add fitMedia line of code
-        cy.get(".ace_content").click()
-        cy.get(".ace_content")
-            .type(Cypress._.repeat("{downarrow}", 10))
-            .type("fitMedia(HIPHOP_FUNKBEAT_001, 1, 1, 9)")
+        cy.contains("div", "playsound.py").click()
 
         // Run new Script
         cy.get("button[id='run-button']").click()
 
         // Confirm success from the ES console
         cy.contains("span", "Script ran successfully")
-
-        // Logout (optional)
-        cy.get("button[id=headlessui-menu-button-11]").click()
-        cy.get("button").contains("Logout").click()
     })
 })
