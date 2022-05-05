54b3d7b01a87433a47ad17690a4e70d96d09551c
heerman <lwheerman@gmail.com>
2022-05-05 16:28:48 -0400

    test language selector

---
+++ tests/e2e-cypress/integration/internationalization.spec.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+describe("language", () => {
+    it("selects language", () => {
+        cy.interceptAudioStandard()
+        cy.visit("/")
+        cy.get("button").contains("Skip").click()
+
+        // select spanish
+        cy.get("button[title='Select Language']").click() // luckily the title is not translated
+        cy.contains("button", "Español").click()
+        cy.contains("div", "GESTOR DE CONTENIDOS")
+
+        // select french
+        cy.get("button[title='Select Language']").click() // luckily the title is not translated
+        cy.contains("button", "Français").click()
+        cy.contains("div", "GESTIONNAIRE DE CONTENU")
+
+        // select english
+        cy.get("button[title='Select Language']").click() // luckily the title is not translated
+        cy.contains("button", "English").click()
+        cy.contains("div", "DIGITAL AUDIO WORKSTATION")
+    })
+})
