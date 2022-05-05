721de1979c4d6389e4729481fceb40d259a3bc4e
heerman <lwheerman@gmail.com>
2022-05-05 17:06:28 -0400

    test blocks mode

--- tests/e2e-cypress/integration/editor.spec.js
+++ tests/e2e-cypress/integration/editor.spec.js
@@ -1,10 +1,26 @@
 /* eslint-disable no-undef */
 describe("Editor", () => {
     beforeEach(() => {
-        cy.interceptAudioStandard()
+        const testSoundMeta = {
+            artist: "ARTIST",
+            folder: "STUB FOLDER",
+            genre: "STUB",
+            genreGroup: "STUB",
+            instrument: "STUB",
+            name: "OS_CLAP01",
+            path: "filename/placeholder/here.wav",
+            public: 1,
+            tempo: -1,
+            year: 2022,
+        }
+
+        cy.interceptAudioStandard([testSoundMeta])
+        cy.interceptAudioMetadata(testSoundMeta)
+        cy.interceptAudioSample()
+
         cy.visit("/")
         cy.get("button").contains("Skip").click()
-        // Create an new script.
+        // Create a new script.
         cy.get("a").contains("Click here to create a new script").click()
         cy.get("#scriptName").type("cypress_test")
         cy.get("input").contains("CREATE").click()
@@ -28,5 +44,23 @@ describe("Editor", () => {
         cy.get(".ace_content").type('{enter}prunt("uh oh")')
         cy.get("button").contains("RUN").click()
         cy.get(".console-error").contains("NameError")
+    })
+
+    it("renders code in blocks mode", () => {
+        cy.get(".ace_content").type(`{selectAll}{del}
+from earsketch import *
+fitMedia(OS_CLAP01, 1, 1, 2)
+if 100 == 100:
+print(5 % 2)
+`)
+        cy.get("button[title='Blocks Mode']").click() // enable blocks
+        cy.get("canvas.droplet-main-canvas").should("be.visible")
+        cy.get("div.droplet-palette-element").should("be.visible")
+        cy.get("button").contains("RUN").click()
+
+        cy.get("button[title='Blocks Mode']").click() // disable blocks
+        cy.get("canvas.droplet-main-canvas").should("not.be.visible")
+        cy.get("div.droplet-palette-element").should("not.be.visible")
+        cy.get("button").contains("RUN").click()
     })
 })
