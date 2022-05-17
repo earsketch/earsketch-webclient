460d50d6dd39e082dafef9113ce95f85fd278165
Stephen Garrett <manodrum@gmail.com>
2022-05-17 14:45:24 -0400

    use `h2` tags for more precision

--- tests/e2e-cypress/integration/internationalization.spec.js
+++ tests/e2e-cypress/integration/internationalization.spec.js
@@ -7,12 +7,12 @@ describe("language", () => {
         // select spanish
         cy.get("button[title='Select Language']").click() // luckily the title is not translated
         cy.contains("button", "Español").click()
-        cy.contains("div", "GESTOR DE CONTENIDOS")
+        cy.contains("h2", "GESTOR DE CONTENIDOS")
 
         // select french
         cy.get("button[title='Select Language']").click() // luckily the title is not translated
         cy.contains("button", "Français").click()
-        cy.contains("div", "GESTIONNAIRE DE CONTENU")
+        cy.contains("h2", "GESTIONNAIRE DE CONTENU")
 
         // select english
         cy.get("button[title='Select Language']").click() // luckily the title is not translated
