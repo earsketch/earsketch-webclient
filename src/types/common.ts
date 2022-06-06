da19859691e7c6b296b495ac449d4bf83322693a
Stephen Garrett <manodrum@gmail.com>
2022-06-06 11:20:32 -0400

    refactor `BrowserTabType` up to common types

--- src/types/common.ts
+++ src/types/common.ts
@@ -96,3 +96,12 @@ export interface DAWData {
     master: GainNode
     slicedClips: { [key: string]: ClipSlice }
 }
+
+export const BrowserTabType = {
+    Sound: 0,
+    Script: 1,
+    API: 2,
+} as const
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export type BrowserTabType = typeof BrowserTabType[keyof typeof BrowserTabType]
