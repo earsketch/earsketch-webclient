162c241b98f605c52bdbe8c3d2e482daad9d9824
Stephen Garrett <manodrum@gmail.com>
2022-06-07 14:24:33 -0400

    remove console.log

--- tests/e2e-cypress/plugins/index.js
+++ tests/e2e-cypress/plugins/index.js
@@ -2,12 +2,10 @@ const { beforeRunHook, afterRunHook } = require("cypress-mochawesome-reporter/li
 
 module.exports = (on) => {
     on("before:run", async (details) => {
-        console.log("override before:run")
         await beforeRunHook(details)
     })
 
     on("after:run", async () => {
-        console.log("override after:run")
         await afterRunHook()
     })
 }
