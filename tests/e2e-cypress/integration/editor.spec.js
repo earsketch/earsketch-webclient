1b2e65ab4410b00597470f33762157053f8cbe21
Sean Tillman <sean.tillman18@gmail.com>
2022-06-10 21:20:08 -0400

    Changed function test to blocks mode test style

--- tests/e2e-cypress/integration/editor.spec.js
+++ tests/e2e-cypress/integration/editor.spec.js
@@ -73,13 +73,10 @@ print(5 % 2)
         cy.get("#scriptName").type("js_test")
         cy.get("input").contains("CREATE").click()
 
-        // Enter fitMedia() call below the setTempo() and run
-        let downArrows = ""
-        const linesDown = 6
-        for (let x = 0; x < linesDown; x++) {
-            downArrows = downArrows + "{downArrow}"
-        }
-        cy.get(".ace_content").type(downArrows + "fitMedia(OS_CLAP01, 1, 1, 2)")
+        // Enter new text with fitMedia()
+        cy.get(".ace_content").type(`{selectAll}{del}{enter}
+fitMedia(OS_CLAP01, 1, 1, 2);
+`)
         cy.get("button").contains("RUN").click()
         cy.get('[data-test="notificationBar"]').contains("Script ran successfully")
     })
