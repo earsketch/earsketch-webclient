4f570a12864409970f157422d0fe95342f6b217e
Stephen Garrett <manodrum@gmail.com>
2022-08-18 11:14:14 -0400

    add dialog title to all modals, increase button contrast

--- src/Utils.tsx
+++ src/Utils.tsx
@@ -1,6 +1,7 @@
 import React, { MutableRefObject, useEffect, useRef, useState } from "react"
 import { useTranslation } from "react-i18next"
 import classNames from "classnames"
+import { Dialog } from "@headlessui/react"
 
 // Useful for preventing absolute-positioned elements from exceeding window height.
 export const useHeightLimiter = (show: boolean, marginBottom: string|null = null): [MutableRefObject<HTMLDivElement|null>, React.CSSProperties] => {
@@ -38,7 +39,7 @@ export const Alert = ({ message }: { message: string }) => {
 
 export const ModalHeader: React.FC = ({ children }) => {
     return <>
-        <div className="border-b p-3.5 text-gray-900 dark:text-white">{children}</div>
+        <Dialog.Title className="border-b p-3.5 text-gray-900 dark:text-white">{children}</Dialog.Title>
     </>
 }
 
@@ -60,7 +61,7 @@ export const ModalFooter = ({ submit, cancel, ready, progress, type, close }: {
     const { t } = useTranslation()
     const btnClass = classNames({
         "btn text-sm py-1.5 px-3 ml-2": true,
-        "bg-sky-600 text-white hover:text-white hover:bg-sky-700 disabled:cursor-not-allowed disabled:opacity-75": !type,
+        "bg-sky-700 text-white hover:text-white hover:bg-sky-800 disabled:cursor-not-allowed disabled:opacity-75": !type,
         "bg-red-600 text-white hover:text-white hover:bg-red-700": type === "danger",
     })
     return <div className="flex items-center justify-end border-t p-3.5">
