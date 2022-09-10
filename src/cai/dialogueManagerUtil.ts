05981fee16efcdc3329213e8fcb2ee1edceebe28
Amogh Mannekote <msamogh@gmail.com>
2022-09-10 20:43:06 +0100

    await

--- src/cai/dialogueManagerUtil.ts
+++ src/cai/dialogueManagerUtil.ts
@@ -48,26 +48,24 @@ export function triggerIntent(message: any) {
     socket.emit("user_did", message)
 }
 
-export function _updateESDialogueState() {
-    fetch(`${RASA_SERVER_URL}/conversations/${CONVERSATION_ID}/tracker?token=rasaToken`, {
+export async function _updateESDialogueState() {
+    const response: any = await fetch(`${RASA_SERVER_URL}/conversations/${CONVERSATION_ID}/tracker?token=rasaToken`, {
         method: "GET",
         headers: {
             mode: "cors",
         },
     })
-    .then((res: any) => {
-        const response = res.json()
-        response.slots.forEach((slot: any) => {
-            if (slot.slot_name === "code_structure") {
-                projectModel.updateModel("code structure", slot.slot_value)
-            } else if (slot.slot_name === "musical_form") {
-                projectModel.updateModel("form", slot.slot_value)
-            } else if (slot.slot_name === "genre") {
-                projectModel.updateModel("genre", slot.slot_value)
-            }
-        })
-        console.log("Updated ES state from Rasa")
+    const tracker = response.json()
+    tracker.slots.forEach((slot: any) => {
+        if (slot.slot_name === "code_structure") {
+            projectModel.updateModel("code structure", slot.slot_value)
+        } else if (slot.slot_name === "musical_form") {
+            projectModel.updateModel("form", slot.slot_value)
+        } else if (slot.slot_name === "genre") {
+            projectModel.updateModel("genre", slot.slot_value)
+        }
     })
+    console.log("Updated ES state from Rasa")
 }
 
 export function sendChatMessageToNLU(messageText: string) {
