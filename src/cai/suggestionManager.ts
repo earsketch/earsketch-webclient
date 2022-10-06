b2c7d553e89f8f77d71416a12f7e86beed5444ba
Jas1238Smith <jas1238smith@gmail.com>
2022-10-06 11:03:19 -0400

    Adjust, reset weights

--- src/cai/suggestionManager.ts
+++ src/cai/suggestionManager.ts
@@ -5,13 +5,29 @@ import { Modules } from "./suggestionModule"
 import { NewCodeModule } from "./suggestNewCode"
 
 const suggestionWeights: { [key in Modules]: number } = {
-    newCode: 0,
-    advanceCode: 0,
-    aesthetics: 0,
+    newCode: 0.33,
+    advanceCode: 0.33,
+    aesthetics: 0.33,
 }
 
-export function adjustWeights(type: Modules, value: number) {
-    suggestionWeights[type] = value
+export function adjustWeights(type: Modules, adjustment: number) {
+    adjustment = Math.min(Math.max(adjustment, -1), 1)
+    const remainder = Object.values(suggestionWeights).reduce((sum, a) => sum + a, 0) - suggestionWeights[type]
+    const adjustedRemainder = remainder - adjustment
+    for (const key of Object.keys(suggestionWeights)) {
+        if (key === type) {
+            suggestionWeights[key] += adjustment
+        } else {
+            suggestionWeights[key as Modules] = suggestionWeights[key as Modules] / remainder * adjustedRemainder
+        }
+    }
+}
+
+export function resetWeights() {
+    const ratio = 1.0 / Object.keys(suggestionWeights).length
+    for (const key of Object.keys(suggestionWeights)) {
+        suggestionWeights[key as Modules] = ratio
+    }
 }
 
 export function generateSuggestion(): CodeRecommendation | null {
