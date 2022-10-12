79a8ecbf94c01ae09dfa28685c51d4b12523f818
Erin Truesdell <nern.strudel@gmail.com>
2022-10-12 13:50:38 -0400

    add skeleton content for curriculum progression storage and suggestNewCode

--- src/cai/suggestNewCode.ts
+++ src/cai/suggestNewCode.ts
@@ -1,6 +1,39 @@
 import { SuggestionModule } from "./suggestionModule"
+import { CodeRecommendation } from "./codeRecommendations"
+import { curriculumProgression } from "./suggestionManager"
 
 export const NewCodeModule: SuggestionModule = {
     name: "newCode",
-    suggestion: () => { return null },
+    suggestion: () => { return generateSuggestion() },
+}
+
+function generateSuggestion(): CodeRecommendation {
+    // empty return to make the linter happy
+    const potentialSuggestionItems: { [key: string]: { [key: string]: number } } [] = []
+    const fromRecentDeltas = findNextCurriculumItems()
+
+    for (const i of fromRecentDeltas) {
+        i["weight"] = { weight: 0.33 }
+        potentialSuggestionItems.push(Object.assign({}, i))
+    }
+
+    return {
+        id: 0, // arbitratry index number to be accessed by suggestion decision tree.
+        utterance: "",
+        explain: "",
+        example: "",
+    }
+}
+
+function findNextCurriculumItems(): { [key: string]: { [key: string]: number } } [] {
+    const newCurriculumItems: { [key: string]: { [key: string]: number } } [] = []
+    const conceptIndices: number[] = []
+    // find indices of 3 most recent deltas, if they exist
+
+    for (const i of conceptIndices) {
+        if (i < 14) {
+            newCurriculumItems.push(Object.assign({}, curriculumProgression[conceptIndices[i + 1]]))
+        }
+    }
+    return newCurriculumItems
 }
--- src/cai/suggestionManager.ts
+++ src/cai/suggestionManager.ts
@@ -4,6 +4,24 @@ import { AestheticsModule } from "./suggestAesthetics"
 import { Modules } from "./suggestionModule"
 import { NewCodeModule } from "./suggestNewCode"
 
+export const curriculumProgression: { [key: number]: { [key: string]: { [key: string]: number } } } = {
+    0: { variables: { variables: 1 } },
+    1: { makeBeat: { makeBeat: 1 } },
+    2: { iteration: { forLoopsRange: 2, forLoopsIterable: 1 } }, // needs special handling as is language-dependent. hrm.
+    3: { features: { mathOps: 1 } },
+    4: { iteration: { forLoopsRange: 3 } },
+    5: { conditionals: { conditionals: 1 } },
+    6: { conditionals: { conditionals: 3 } },
+    7: { features: { comparisons: 1 } },
+    8: { functions: { repeatExecution: 1 } },
+    9: { functions: { repeatExecution: 3 } },
+    10: { functions: { manipulateValue: 3 } },
+    11: { features: { consoleInput: 1 } },
+    12: { features: { strOps: 1 } },
+    13: { features: { indexing: 1 } },
+    14: { makeBeat: { makeBeat: 3 } },
+}
+
 const suggestionWeights: { [key in Modules]: number } = {
     newCode: 0.33,
     advanceCode: 0.33,
