da21e43c9b021ae1bbe47772b14d8ab14c7b9e1e
Stephen Garrett <manodrum@gmail.com>
2022-10-13 12:46:30 -0400

    describe color theme state in theme button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -516,9 +516,11 @@ const FontSizeMenu = () => {
 
 const SwitchThemeButton = () => {
     const { t } = useTranslation()
+    const colorTheme = useSelector(appState.selectColorTheme)
+    const titleKey = colorTheme === "light" ? "switchThemeLight" : "switchThemeDark"
 
     return <div className="relative inline-block text-left mx-3">
-        <button className="text-gray-400 hover:text-gray-300 text-2xl" onClick={toggleColorTheme} title={t("switchTheme")} aria-label={t("switchTheme")}>
+        <button className="text-gray-400 hover:text-gray-300 text-2xl" onClick={toggleColorTheme} title={t(titleKey)} aria-label={t(titleKey)}>
             <div className="flex flex-row items-center">
                 <div><i className="icon icon-brightness-contrast" /></div>
             </div>
