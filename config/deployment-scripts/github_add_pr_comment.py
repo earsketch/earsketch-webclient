7f714e57346dccc753b7cb2543974dcde4b3072e
Stephen Garrett <manodrum@gmail.com>
2022-10-21 13:39:44 -0400

    add sha argument

--- config/deployment-scripts/github_add_pr_comment.py
+++ config/deployment-scripts/github_add_pr_comment.py
@@ -3,16 +3,18 @@ import sys
 import requests
 from requests.auth import HTTPBasicAuth
 
-if len(sys.argv) < 5:
+if len(sys.argv) < 6:
     print("Error, no arguments given")
     print(
-        "Usage: github_create_release.py <GIT_USER> <GITHUB_TOKEN> <BUILD_NUMBER> <PULL_REQUEST_NUMBER>"
+        "Usage: github_create_release.py <GIT_USER> <GITHUB_TOKEN> <BUILD_NUMBER> <PULL_REQUEST_NUMBER> "
+        "<GIT_COMMIT_SHA> "
     )
     exit(1)
 github_user = sys.argv[1]
 github_token = sys.argv[2]
 build_number = sys.argv[3]
 pull_request_number = sys.argv[4].replace("pr-", "")
+commit_sha = sys.argv[5]
 
 url = (
     "https://api.github.com/repos/GTCMT/earsketch-webclient/issues/"
