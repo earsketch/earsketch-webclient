f13a4b52ef6d3f5a9b1e8b2edceed8188f7867aa
Jas1238Smith <jas1238smith@gmail.com>
2022-11-07 11:32:04 -0500

    Move generateSuggestion inside Module

--- src/cai/suggestAdvanceCode.ts
+++ src/cai/suggestAdvanceCode.ts
@@ -1,95 +1,74 @@
 import { SuggestionModule, curriculumProgression } from "./suggestionModule"
 import { studentModel } from "./student"
-import { getApiCalls } from "./complexityCalculator"
+import { getApiCalls, CodeFeatures } from "./complexityCalculator"
 import { CodeRecommendation } from "./codeRecommendations"
-import  store  from "../reducers"
+import store from "../reducers"
 import * as caiState from "./caiState"
 import { savedReport } from "./analysis"
-import { CodeFeatures } from "./complexityCalculator"
 import { state as ccstate } from "./complexityCalculatorState"
-
-
 
 export const AdvanceCodeModule: SuggestionModule = {
     weight: 0,
-    suggestion: () => { return generateSuggestion() },
-}
+    suggestion: () => {
+        // // data that can be accessed
+        // console.log("soundProfile",studentModel.musicAttributes.soundProfile) // effect, measure, numberofsubsec, sounds, subsec
+        // console.log("saved report", savedReport) // apicalls, measureview, mixing, overview, soundprofile, variables
+        // console.log(store.getState(),caiState.selectActiveProject(store.getState())) // need to parse
+        // console.log("full ccstate", ccstate) // allVar, apiCalls, codeStructure, functionLines, listFuncs, loopLocations, strFuncs, studentCode, uncalledFunctionLines, userFunctionReturns
 
-function generateSuggestion(): CodeRecommendation {
+        // // to use
+        // console.log("api calls", getApiCalls()) // --> need calls
+        // console.log("aggregate complexity", studentModel.codeKnowledge.aggregateComplexity) // -> need
+        // console.log("project delta", caiState.selectProjectHistories(store.getState())[caiState.selectActiveProject(store.getState())]) // history of changes? --> need
+        // console.log("curriculum", studentModel.codeKnowledge.curriculum) // --> tracks pages opened
+        // console.log("curriculum progression", curriculumProgression) // --> follow newCode
+        // console.log("custom functions", ccstate.userFunctionReturns) // --> correct
 
-    // // data that can be accessed
-    // console.log("soundProfile",studentModel.musicAttributes.soundProfile) // effect, measure, numberofsubsec, sounds, subsec
-    // console.log("saved report", savedReport) // apicalls, measureview, mixing, overview, soundprofile, variables
-    // console.log(store.getState(),caiState.selectActiveProject(store.getState())) // need to parse
-    // console.log("full ccstate", ccstate) // allVar, apiCalls, codeStructure, functionLines, listFuncs, loopLocations, strFuncs, studentCode, uncalledFunctionLines, userFunctionReturns
-
-    // // to use
-    // console.log("api calls", getApiCalls()) // --> need calls
-    // console.log("aggregate complexity", studentModel.codeKnowledge.aggregateComplexity) // -> need
-    // console.log("project delta", caiState.selectProjectHistories(store.getState())[caiState.selectActiveProject(store.getState())]) // history of changes? --> need
-    // console.log("curriculum", studentModel.codeKnowledge.curriculum) // --> tracks pages opened
-    // console.log("curriculum progression", curriculumProgression) // --> follow newCode
-    // console.log("custom functions", ccstate.userFunctionReturns) // --> correct
-
-    
-
-    const state = store.getState()
-
-    return tree();
-}
-
-function tree(): CodeRecommendation {
-
-
-    var suggestion = {
-        id: 0,  // arbitratry index number to be accessed by suggestion decision tree.
-        utterance: "",
-        explain: "",
-        example: "",
-    }
-
-    // check if function in code vs what sound complexity found
-    if(Object.keys(studentModel.musicAttributes.soundProfile).length > 1 && ccstate.userFunctionReturns.length == 0) {
-        suggestion.utterance = "I think you should write a function with the section you have already created"
-    }
-
-    // works but unknown functions listed as well
-    for(let i = 0; i < ccstate.userFunctionReturns.length; i ++ ) {
-        if(ccstate.userFunctionReturns[i].calls.length == 0) {
-            suggestion.utterance = "I think you can modularize your code by calling " + ccstate.userFunctionReturns[i].name + " at least once";
+        const state = store.getState()
+        const suggestion = {
+            id: 0, // arbitratry index number to be accessed by suggestion decision tree.
+            utterance: "",
+            explain: "",
+            example: "",
         }
-    }
-    
-    // suggestions calling functions at least twice
-    for(let i = 0; i < ccstate.userFunctionReturns.length; i++ ) {
-        if(ccstate.userFunctionReturns[i].calls.length == 1) {
-            suggestion.utterance = "Functions can be helpful when they're used multiple times, maybe consider using", ccstate.userFunctionReturns[i].name, " again";
+
+        // check if function in code vs what sound complexity found
+        if (Object.keys(studentModel.musicAttributes.soundProfile).length > 1 && ccstate.userFunctionReturns.length === 0) {
+            suggestion.utterance = "I think you should write a function with the section you have already created"
         }
-    }
 
-    // // variables from other files here? not sure from where
-    // // needs count of where a variable is actually called, similar to custom function calls...
-    // for(let i = 0; i < ccstate.allVariables.length; i++ ) {
-    //     if(ccstate.allVariables[i].calls.length == 0) {
-    //         suggestion.utterance = "there's a defined variable but it hasn't been called yet: ", ccstate.allVariables[i].name;
-    //     }
-    // }
-
-    // check for repeated code with fitMedia and suggest a loop
-    let apiCalls = [];
-    apiCalls = Object.assign(getApiCalls(), []);
-    apiCalls.sort((a,b) => (a.clips[0] <= b.clips[0] ? 1 : -1))
-    apiCalls = apiCalls.filter( (a) => { return a.function == "fitMedia"})
-    // console.log("sorted calls")
-    for(let i = 2; i < apiCalls.length; i ++ ) {
-        if(apiCalls[i].clips[0] == apiCalls[i-1].clips[0] && apiCalls[i].clips[0] == apiCalls[i-2].clips[0]) {
-            suggestion.utterance = "maybe try using a loop since you have a few fitMedias using " + apiCalls[i].clips[0]
-            break;
+        // works but unknown functions listed as well
+        for (const func of ccstate.userFunctionReturns) {
+            if (func.calls.length === 0) {
+                suggestion.utterance = "I think you can modularize your code by calling " + func.name + " at least once"
+            } else if (func.calls.length === 1) {
+                suggestion.utterance = "Functions can be helpful when they're used multiple times, maybe consider using" + func.name + " again"
+            }
         }
-    }
 
+        // // variables from other files here? not sure from where
+        // // needs count of where a variable is actually called, similar to custom function calls...
+        // for(let i = 0; i < ccstate.allVariables.length; i++ ) {
+        //     if(ccstate.allVariables[i].calls.length === 0) {
+        //         suggestion.utterance = "there's a defined variable but it hasn't been called yet: ", ccstate.allVariables[i].name;
+        //     }
+        // }
 
-    // console.log(suggestion.utterance);
+        // check for repeated code with fitMedia and suggest a loop
+        let apiCalls = []
+        apiCalls = Object.assign(getApiCalls(), [])
+        apiCalls.sort((a, b) => (a.clips[0] <= b.clips[0] ? 1 : -1))
+        apiCalls = apiCalls.filter((a) => { return a.function === "fitMedia" })
+        // console.log("sorted calls")
+        for (let i = 2; i < apiCalls.length; i++) {
+            if (apiCalls[i].clips[0] === apiCalls[i - 1].clips[0] && apiCalls[i].clips[0] === apiCalls[i - 2].clips[0]) {
+                suggestion.utterance = "maybe try using a loop since you have a few fitMedias using " + apiCalls[i].clips[0]
+                break
+            }
+        }
 
-    return suggestion;
+        // console.log(suggestion.utterance);
+
+        return suggestion
+    },
 }
--- src/cai/suggestNewCode.ts
+++ src/cai/suggestNewCode.ts
@@ -5,13 +5,7 @@ import { CodeFeatures } from "./complexityCalculator"
 import store from "../reducers"
 import { getModel } from "./projectModel"
 
-export const NewCodeModule: SuggestionModule = {
-    weight: 0,
-    suggestion: () => { return generateSuggestion() },
-}
-
 const suggestionContent: { [key: string]: CodeRecommendation } = {
-
     0: { id: 200, utterance: "why don't we use [LINK|variables] to name our sounds so it's easier to swap in new sounds?", explain: "", example: "" },
     1: { id: 201, utterance: "we can use [LINK|makeBeat]() to create our own beat for our song", explain: "[LINK|makeBeat] lets us use a string to put a beat we want in our song", example: "something like " },
     2: { id: 202, utterance: "using a [LINK|for loop] can help us repeat code without having to write it a bunch of times", explain: "", example: "" },
@@ -22,101 +16,105 @@ const suggestionContent: { [key: string]: CodeRecommendation } = {
     13: { id: 213, utterance: "using a list with indexing, we could use a version of [LINK|makeBeat] that includes different sounds in the beat", explain: "", example: "" },
 }
 
-function generateSuggestion(): CodeRecommendation {
-    const potentialSuggestionItems: { [key: number]: number } = {}
-    const fromRecentDeltas = findNextCurriculumItems()
-    const fromOtherProjects = nextItemsFromPreviousProjects()
-    const currentState: CodeFeatures = selectProjectHistories(store.getState())[selectActiveProject(store.getState())][0]
+export const NewCodeModule: SuggestionModule = {
+    weight: 0,
+    suggestion: () => {
+        const state = store.getState()
+        const potentialSuggestionItems: { [key: number]: number } = {}
+        const fromRecentDeltas = findNextCurriculumItems()
+        const fromOtherProjects = nextItemsFromPreviousProjects()
+        const currentState: CodeFeatures = selectProjectHistories(state)[selectActiveProject(state)][0]
 
-    // create objects with weight for each topic. add weight to "next in project" topic and topics from "fromOtherProjects" array
-    // set up with default weights, then modify, then adjust
-    for (const i of fromRecentDeltas) {
-        potentialSuggestionItems[i] = 0.1
-    }
-    for (const i of fromOtherProjects) {
-        if (potentialSuggestionItems[i]) {
-            potentialSuggestionItems[i] += 0.15
-        } else {
-            potentialSuggestionItems[i] = 0.15
+        // create objects with weight for each topic. add weight to "next in project" topic and topics from "fromOtherProjects" array
+        // set up with default weights, then modify, then adjust
+        for (const i of fromRecentDeltas) {
+            potentialSuggestionItems[i] = 0.1
         }
-    }
-
-    // add weights
-    let highestConcept = 0
-    let conceptIndex = 0
-    while (conceptIndex < 15) {
-        for (const curricConcept in curriculumProgression[conceptIndex]) {
-            for (const curricTopic in curriculumProgression[conceptIndex][curricConcept]) {
-                if (currentState[curricConcept][curricTopic] > 0) {
-                    highestConcept = conceptIndex
-                }
+        for (const i of fromOtherProjects) {
+            if (potentialSuggestionItems[i]) {
+                potentialSuggestionItems[i] += 0.15
+            } else {
+                potentialSuggestionItems[i] = 0.15
             }
         }
-        conceptIndex++
-    }
 
-    // adjust weights
+        // add weights
+        let highestConcept = 0
+        let conceptIndex = 0
+        while (conceptIndex < 15) {
+            for (const curricConcept in curriculumProgression[conceptIndex]) {
+                for (const curricTopic in curriculumProgression[conceptIndex][curricConcept]) {
+                    if (currentState[curricConcept][curricTopic] > 0) {
+                        highestConcept = conceptIndex
+                    }
+                }
+            }
+            conceptIndex++
+        }
 
-    // "next in project"
-    highestConcept += 1
-    while (!potentialSuggestionItems[highestConcept] && highestConcept <= 13) {
+        // adjust weights
+
+        // "next in project"
         highestConcept += 1
-    }
-    if (highestConcept <= 13) {
-        potentialSuggestionItems[highestConcept] += 0.1
-    }
+        while (!potentialSuggestionItems[highestConcept] && highestConcept <= 13) {
+            highestConcept += 1
+        }
+        if (highestConcept <= 13) {
+            potentialSuggestionItems[highestConcept] += 0.1
+        }
 
-    // get project goals
-    for (const suggItem in potentialSuggestionItems) {
+        // get project goals
+        for (const suggItem in potentialSuggestionItems) {
         // if it's an unmet project model goal, increase weight
         // use key to get curriculum prog
-        const curricObj = curriculumProgression[suggItem]
-        for (const curricConcept in curricObj) {
-            for (const curricTopic in curricObj[curricConcept]) {
+            const curricObj = curriculumProgression[suggItem]
+            for (const curricConcept in curricObj) {
+                for (const curricTopic in curricObj[curricConcept]) {
                 // does the concept match anything in the goal model?
-                if (getModel().complexityGoals[curricConcept][curricTopic] === curricObj[curricConcept][curricTopic]) {
+                    if (getModel().complexityGoals[curricConcept][curricTopic] === curricObj[curricConcept][curricTopic]) {
                     // is it unmet
-                    if (curricObj[curricConcept][curricTopic] > currentState[curricConcept][curricTopic]) {
+                        if (curricObj[curricConcept][curricTopic] > currentState[curricConcept][curricTopic]) {
                         // if it is unmet, add weight. also, break.
-                        potentialSuggestionItems[suggItem] += 0.2
-                        break
+                            potentialSuggestionItems[suggItem] += 0.2
+                            break
+                        }
                     }
                 }
             }
         }
-    }
 
-    // scale weights
-    let weightTotal = 0
-    for (const weightedItem in potentialSuggestionItems) {
-        weightTotal += potentialSuggestionItems[weightedItem]
-    }
-    const multiplier = 1 / weightTotal
-    for (const weightedItem in potentialSuggestionItems) {
-        potentialSuggestionItems[weightedItem] *= multiplier
-    }
+        // scale weights
+        let weightTotal = 0
+        for (const weightedItem in potentialSuggestionItems) {
+            weightTotal += potentialSuggestionItems[weightedItem]
+        }
+        const multiplier = 1 / weightTotal
+        for (const weightedItem in potentialSuggestionItems) {
+            potentialSuggestionItems[weightedItem] *= multiplier
+        }
 
-    // select weighted random
-    if (Object.keys(potentialSuggestionItems).length > 0) {
-        const suggs = Object.keys(potentialSuggestionItems)
+        // select weighted random
+        if (Object.keys(potentialSuggestionItems).length > 0) {
+            const suggs = Object.keys(potentialSuggestionItems)
 
-        // create cumulative list of weighted sums, then generate a random number in that range.
-        let sum: number = 0
-        const cumulativeWeights = suggs.map((a) => {
-            sum += potentialSuggestionItems[a]
-            return sum
-        })
-        const randomNumber = Math.random() * sum
-        let suggIndex: string = "0"
-        // return the module with weight range containing the randomly selected number.
-        suggs.forEach((module, idx) => {
-            if (cumulativeWeights[idx] >= randomNumber) {
-                suggIndex = module
-            }
-        })
-        return suggestionContent[suggIndex]
-    }
-    return suggestionContent[0]
+            // create cumulative list of weighted sums, then generate a random number in that range.
+            let sum: number = 0
+            const cumulativeWeights = suggs.map((a) => {
+                sum += potentialSuggestionItems[a]
+                return sum
+            })
+            const randomNumber = Math.random() * sum
+            let suggIndex: string = "0"
+            // return the module with weight range containing the randomly selected number.
+            suggs.forEach((module, idx) => {
+                if (cumulativeWeights[idx] >= randomNumber) {
+                    suggIndex = module
+                }
+            })
+            return suggestionContent[suggIndex]
+        }
+        return suggestionContent[0]
+    },
 }
 
 function findNextCurriculumItems(): number [] {
@@ -158,7 +156,8 @@ function findNextCurriculumItems(): number [] {
 }
 
 function currentProjectDeltas(): { [key: string]: { [key: string]: number } } [] {
-    const projectHistory: CodeFeatures[] = selectProjectHistories(store.getState())[selectActiveProject(store.getState())]
+    const state = store.getState()
+    const projectHistory: CodeFeatures[] = selectProjectHistories(state)[selectActiveProject(state)]
     console.log(projectHistory)
     const projectDeltas: { [key: string]: { [key: string]: number } } [] = []
     // get and then sort and then filter output from the histroy
@@ -211,8 +210,8 @@ function nextItemsFromPreviousProjects(): number[] {
             }
         }
     }
-    // rank used concepts by usage amount
 
+    // rank used concepts by usage amount
     const allConceptsSorted = Object.entries(allConcepts).sort((a, b) => a[1] - b[1])
 
     // add least-used concepts to return values list IF there's a corresponding suggestion.
