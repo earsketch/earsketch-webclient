847af4aa615087dd2b429a10f53caa15b2c283e9
Jas1238Smith <jas1238smith@gmail.com>
2022-11-07 20:16:55 -0500

    projectModel-based Aesthetics logic

--- src/cai/suggestAesthetics.ts
+++ src/cai/suggestAesthetics.ts
@@ -1,6 +1,7 @@
 import { SuggestionModule } from "./suggestionModule"
-import { savedReport } from "./analysis"
-import { CAI_RECOMMENDATIONS, CAI_NUCLEI } from "./codeRecommendations"
+import { soundDict } from "../app/recommender"
+import { savedReport, soundProfileLookup } from "./analysis"
+import { CAI_RECOMMENDATIONS, CAI_NUCLEI, CodeRecommendation } from "./codeRecommendations"
 import { getModel } from "./projectModel"
 import * as caiState from "./caiState"
 import store from "../reducers"
@@ -11,27 +12,70 @@ export const AestheticsModule: SuggestionModule = {
         const state = store.getState()
         const activeProject = caiState.selectActiveProject(state)
         const projectModel = getModel()
+        const possibleSuggestions: CodeRecommendation[] = []
 
         if (savedReport.OVERVIEW.measures === 0) {
             // TODO: replace messageList with list of suggested sounds via caiState.
             if (caiState.selectMessageList(state)[activeProject].length === 0) {
                 // Suggest a starting sound
-                return CAI_NUCLEI.oneSound
+                possibleSuggestions.push(CAI_NUCLEI.oneSound)
             } else {
                 // Suggest one, two, or three sounds
-                return randomSoundSuggestion()
+                possibleSuggestions.push(randomSoundSuggestion())
             }
         }
 
-        if (projectModel.musicalProperties.instruments.length === 0) {
-            return CAI_RECOMMENDATIONS.instrument
+        // If project is shorter than requirements, recommend adding new sounds/sections.
+        if (savedReport.OVERVIEW.measures < projectModel.musicalProperties.lengthMeasures ||
+            savedReport.OVERVIEW["length (seconds)"] < projectModel.musicalProperties.lengthSeconds) {
+            possibleSuggestions.push({
+                id: 0,
+                utterance: "",
+                explain: "",
+                example: "",
+            })
         }
 
-        if (projectModel.api.setEffect === 0) {
-            return CAI_RECOMMENDATIONS.effect
+        // Suggest instrument from project model in a section lacking that instrument.
+        const instrumentRecommendations: CodeRecommendation[] = []
+        for (const instrument of projectModel.musicalProperties.instruments) {
+            for (const section of Object.keys(savedReport.SOUNDPROFILE)) {
+                const sounds = soundProfileLookup(savedReport.SOUNDPROFILE, "section", section, "sound")
+                if (sounds.map((s) => { return soundDict[s].instrument }).includes(instrument)) {
+                    const measures = soundProfileLookup(savedReport.SOUNDPROFILE, "section", section, "measure")
+                    instrumentRecommendations.push({
+                        id: 0,
+                        utterance: "Measures " + measures[0] + "-" + measures[1] + " could use some " + instrument + " sounds.",
+                        explain: "",
+                        example: "",
+                    })
+                }
+            }
+        }
+        if (instrumentRecommendations.length) {
+            possibleSuggestions.push(instrumentRecommendations[Math.random() * instrumentRecommendations.length])
         }
 
-        return randomSoundSuggestion()
+        // Compare current form against project model form goal.
+        if (projectModel.musicalProperties.form) {
+            const form = Object.keys(savedReport).join("").replace(/^[a-zA-Z]+$/, "")
+            if (form !== projectModel.musicalProperties.form) {
+                possibleSuggestions.push({
+                    id: 0,
+                    utterance: "We want " + projectModel.musicalProperties.form + " but our project looks more like " + form + ". " +
+                    "How about adding a new section?",
+                    explain: "",
+                    example: "",
+                })
+            }
+        }
+
+        // Suggest effects.
+        if (projectModel.api.setEffect) {
+            possibleSuggestions.push(CAI_RECOMMENDATIONS.effect)
+        }
+
+        return possibleSuggestions[Math.random() * possibleSuggestions.length]
     },
 }
 
