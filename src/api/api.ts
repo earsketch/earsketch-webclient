71ae2e82957d13d88f018fc6c1bd8b6d04aead73
Ian Clester <ijc@ijc8.me>
2022-11-09 17:51:12 -0500

    Remove redundant key literals from `api` (part 2)

--- src/api/api.ts
+++ src/api/api.ts
@@ -60,24 +60,19 @@ export const EFFECT_NAMES = [
 ]
 export const ANALYSIS_NAMES = ["SPECTRAL_CENTROID", "RMS_AMPLITUDE"]
 
-interface Parameter {
+type Parameter = string | { type: string, default: string }
+
+export interface APIParameter {
     typeKey: string
+    descriptionKey: string
     default?: string
 }
 
-export interface APIParameter extends Parameter {
-    descriptionKey: string
-}
-
 interface Item {
-    example: {
-        pythonKey: string
-        javascriptKey: string
-    }
     parameters?: { [name: string]: Parameter }
     returns?: string
     language?: string
-    deprecated?: true
+    deprecated?: boolean
 }
 
 export interface APIItem {
@@ -101,497 +96,224 @@ export interface APIItem {
 const rawDoc: { [key: string]: Item[] } = {
     analyze: [{
         parameters: {
-            sound: {
-                typeKey: "api:types.soundConstant",
-            },
-
-            feature: {
-                typeKey: "api:types.analysisConstant",
-            },
+            sound: "soundConstant",
+            feature: "analysisConstant",
         },
         returns: "float",
-        example: {
-            pythonKey: "api:analyze.example.python",
-            javascriptKey: "api:analyze.example.javascript",
-        },
     }],
     analyzeForTime: [{
         parameters: {
-            sound: {
-                typeKey: "api:types.soundConstant",
-            },
-            feature: {
-                typeKey: "api:types.analysisConstant",
-            },
-            sliceStart: {
-                typeKey: "api:types.float",
-            },
-            sliceEnd: {
-                typeKey: "api:types.float",
-            },
+            sound: "soundConstant",
+            feature: "analysisConstant",
+            sliceStart: "float",
+            sliceEnd: "float",
         },
         returns: "float",
-        example: {
-            pythonKey: "api:analyzeForTime.example.python",
-            javascriptKey: "api:analyzeForTime.example.javascript",
-        },
     }],
     analyzeTrack: [{
         parameters: {
-            track: {
-                typeKey: "api:types.integer",
-            },
-            feature: {
-                typeKey: "api:types.analysisConstant",
-            },
+            track: "integer",
+            feature: "analysisConstant",
         },
         returns: "float",
-        example: {
-            pythonKey: "api:analyzeTrack.example.python",
-            javascriptKey: "api:analyzeTrack.example.javascript",
-        },
     }],
     analyzeTrackForTime: [{
         parameters: {
-            track: {
-                typeKey: "api:types.integer",
-            },
-            feature: {
-                typeKey: "api:types.analysisConstant",
-            },
-            start: {
-                typeKey: "api:types.float",
-            },
-            end: {
-                typeKey: "api:types.float",
-            },
+            track: "integer",
+            feature: "analysisConstant",
+            start: "float",
+            end: "float",
         },
         returns: "float",
-        example: {
-            pythonKey: "api:analyzeTrackForTime.example.python",
-            javascriptKey: "api:analyzeTrackForTime.example.javascript",
-        },
     }],
     createAudioSlice: [{
         parameters: {
-            sound: {
-                typeKey: "api:types.soundConstant",
-            },
-            sliceStart: {
-                typeKey: "api:types.float",
-            },
-            sliceEnd: {
-                typeKey: "api:types.float",
-            },
-        },
-        example: {
-            pythonKey: "api:createAudioSlice.example.python",
-            javascriptKey: "api:createAudioSlice.example.javascript",
+            sound: "soundConstant",
+            sliceStart: "float",
+            sliceEnd: "float",
         },
         returns: "soundConstant",
     }],
     dur: [{
         parameters: {
-            sound: {
-                typeKey: "api:types.soundConstant",
-            },
-        },
-        example: {
-            pythonKey: "api:dur.example.python",
-            javascriptKey: "api:dur.example.javascript",
+            sound: "soundConstant",
         },
         returns: "float",
     }],
     finish: [{
-        example: {
-            pythonKey: "api:finish.example.python",
-            javascriptKey: "api:finish.example.javascript",
-        },
         deprecated: true,
     }],
     fitMedia: [{
         parameters: {
-            sound: {
-                typeKey: "api:types.soundConstant",
-            },
-            track: {
-                typeKey: "api:types.integer",
-            },
-            start: {
-                typeKey: "api:types.float",
-            },
-            end: {
-                typeKey: "api:types.float",
-            },
-        },
-        example: {
-            pythonKey: "api:fitMedia.example.python",
-            javascriptKey: "api:fitMedia.example.javascript",
+            sound: "soundConstant",
+            track: "integer",
+            start: "float",
+            end: "float",
         },
     }],
     importImage: [{
         parameters: {
-            url: {
-                typeKey: "api:types.string",
-            },
-            nrows: {
-                typeKey: "api:types.integer",
-            },
-            ncols: {
-                typeKey: "api:types.integer",
-            },
+            url: "string",
+            nrows: "integer",
+            ncols: "integer",
             includeRGB: {
-                typeKey: "api:types.booleanOptional",
+                type: "booleanOptional",
                 default: "False",
             },
-        },
-        example: {
-            pythonKey: "api:importImage.example.python",
-            javascriptKey: "api:importImage.example.javascript",
         },
         returns: "list",
     }],
     importFile: [{
         parameters: {
-            url: {
-                typeKey: "api:types.string",
-            },
+            url: "string",
         },
         returns: "string",
-        example: {
-            pythonKey: "api:importFile.example.python",
-            javascriptKey: "api:importFile.example.javascript",
-        },
     }],
     init: [{
-        example: {
-            pythonKey: "api:init.example.python",
-            javascriptKey: "api:init.example.javascript",
-        },
         deprecated: true,
     }],
     insertMedia: [{
         parameters: {
-            sound: {
-                typeKey: "api:types.soundConstant",
-            },
-            track: {
-                typeKey: "api:types.integer",
-            },
-            start: {
-                typeKey: "api:types.float",
-            },
-        },
-        example: {
-            pythonKey: "api:insertMedia.example.python",
-            javascriptKey: "api:insertMedia.example.javascript",
+            sound: "soundConstant",
+            track: "integer",
+            start: "float",
         },
     }],
     insertMediaSection: [{
         parameters: {
-            sound: {
-                typeKey: "api:types.soundConstant",
-            },
-            track: {
-                typeKey: "api:types.integer",
-            },
-            start: {
-                typeKey: "api:types.float",
-            },
-            sliceStart: {
-                typeKey: "api:types.float",
-            },
-            sliceEnd: {
-                typeKey: "api:types.float",
-            },
-        },
-        example: {
-            pythonKey: "api:insertMediaSection.example.python",
-            javascriptKey: "api:insertMediaSection.example.javascript",
+            sound: "soundConstant",
+            track: "integer",
+            start: "float",
+            sliceStart: "float",
+            sliceEnd: "float",
         },
     }],
     makeBeat: [{
         parameters: {
-            sound: {
-                typeKey: "api:types.soundOrList",
-            },
-            track: {
-                typeKey: "api:types.integer",
-            },
-            start: {
-                typeKey: "api:types.float",
-            },
-            beat: {
-                typeKey: "api:types.string",
-            },
-        },
-        example: {
-            pythonKey: "api:makeBeat.example.python",
-            javascriptKey: "api:makeBeat.example.javascript",
+            sound: "soundOrList",
+            track: "integer",
+            start: "float",
+            beat: "string",
         },
     }],
     makeBeatSlice: [{
         parameters: {
-            sound: {
-                typeKey: "api:types.soundConstant",
-            },
-            track: {
-                typeKey: "api:types.integer",
-            },
-            start: {
-                typeKey: "api:types.float",
-            },
-            beat: {
-                typeKey: "api:types.string",
-            },
-            sliceStarts: {
-                typeKey: "api:types.listArray",
-            },
-        },
-        example: {
-            pythonKey: "api:makeBeatSlice.example.python",
-            javascriptKey: "api:makeBeatSlice.example.javascript",
+            sound: "soundConstant",
+            track: "integer",
+            start: "float",
+            beat: "string",
+            sliceStarts: "listArray",
         },
     }],
     print: [{
         parameters: {
-            input: {
-                typeKey: "api:types.any",
-            },
-        },
-        example: {
-            pythonKey: "api:print.example.python",
-            javascriptKey: "should not show",
+            input: "any",
         },
         language: "python",
     }],
     println: [{
         parameters: {
-            input: {
-                typeKey: "api:types.any",
-            },
-        },
-        example: {
-            pythonKey: "should not show",
-            javascriptKey: "api:println.example.javascript",
+            input: "any",
         },
         language: "javascript",
     }],
     readInput: [{
         parameters: {
-            prompt: {
-                typeKey: "api:types.stringOptional",
-            },
+            prompt: "stringOptional",
         },
         returns: "string",
-        example: {
-            pythonKey: "api:readInput.example.python",
-            javascriptKey: "api:readInput.example.javascript",
-        },
     }],
     replaceListElement: [{
         parameters: {
-            list: {
-                typeKey: "api:types.listArray",
-            },
-            elementToReplace: {
-                typeKey: "api:types.any",
-            },
-            withElement: {
-                typeKey: "api:types.any",
-            },
-        },
-        example: {
-            pythonKey: "api:replaceListElement.example.python",
-            javascriptKey: "api:replaceListElement.example.javascript",
+            list: "listArray",
+            elementToReplace: "any",
+            withElement: "any",
         },
     }],
     replaceString: [{
         parameters: {
-            string: {
-                typeKey: "api:types.string",
-            },
-            characterToReplace: {
-                typeKey: "api:types.string",
-            },
-            withCharacter: {
-                typeKey: "api:types.string",
-            },
+            string: "string",
+            characterToReplace: "string",
+            withCharacter: "string",
         },
         returns: "string",
-        example: {
-            pythonKey: "api:replaceString.example.python",
-            javascriptKey: "api:replaceString.example.javascript",
-        },
     }],
     reverseList: [{
         parameters: {
-            list: {
-                typeKey: "api:types.listArray",
-            },
+            list: "listArray",
         },
         returns: "listArray",
-        example: {
-            pythonKey: "api:reverseList.example.python",
-            javascriptKey: "api:reverseList.example.javascript",
-        },
     }],
     reverseString: [{
         parameters: {
-            string: {
-                typeKey: "api:types.string",
-            },
+            string: "string",
         },
         returns: "string",
-        example: {
-            pythonKey: "api:reverseString.example.python",
-            javascriptKey: "api:reverseString.example.javascript",
-        },
     }],
     rhythmEffects: [{
         parameters: {
-            track: {
-                typeKey: "api:types.integer",
-            },
-            type: {
-                typeKey: "api:types.effectConstant",
-            },
-            parameter: {
-                typeKey: "api:types.effectParameterConstant",
-            },
-            list: {
-                typeKey: "api:types.listArray",
-            },
-            start: {
-                typeKey: "api:types.float",
-            },
-            beat: {
-                typeKey: "api:types.string",
-            },
-        },
-        example: {
-            pythonKey: "api:rhythmEffects.example.python",
-            javascriptKey: "api:rhythmEffects.example.javascript",
+            track: "integer",
+            type: "effectConstant",
+            parameter: "effectParameterConstant",
+            list: "listArray",
+            start: "float",
+            beat: "string",
         },
     }],
     selectRandomFile: [{
         parameters: {
             folderSubstring: {
-                typeKey: "api:types.string",
-                default: "\"\"",
+                type: "string",
+                default: '""',
             },
         },
         returns: "soundConstant",
-        example: {
-            pythonKey: "api:selectRandomFile.example.python",
-            javascriptKey: "api:selectRandomFile.example.javascript",
-        },
     }],
     setEffect: [{
         parameters: {
-            track: {
-                typeKey: "api:types.integer",
-            },
-            type: {
-                typeKey: "api:types.effectConstant",
-            },
-            parameter: {
-                typeKey: "api:types.effectParameterConstant",
-            },
-            value: {
-                typeKey: "api:types.float",
-            },
-        },
-        example: {
-            pythonKey: "api:setEffect1.example.python",
-            javascriptKey: "api:setEffect1.example.javascript",
+            track: "integer",
+            type: "effectConstant",
+            parameter: "effectParameterConstant",
+            value: "float",
         },
     }, {
         parameters: {
-            track: {
-                typeKey: "api:types.integer",
-            },
-            type: {
-                typeKey: "api:types.effectConstant",
-            },
-            parameter: {
-                typeKey: "api:types.effectParameterConstant",
-            },
-            startValue: {
-                typeKey: "api:types.float",
-            },
-            start: {
-                typeKey: "api:types.float",
-            },
-            endValue: {
-                typeKey: "api:types.floatOptional",
-            },
-            end: {
-                typeKey: "api:types.floatOptional",
-            },
-        },
-        example: {
-            pythonKey: "api:setEffect2.example.python",
-            javascriptKey: "api:setEffect2.example.javascript",
+            track: "integer",
+            type: "effectConstant",
+            parameter: "effectParameterConstant",
+            startValue: "float",
+            start: "float",
+            endValue: "floatOptional",
+            end: "floatOptional",
         },
     }],
     setTempo: [{
         parameters: {
-            tempo: {
-                typeKey: "api:types.float",
-            },
-        },
-        example: {
-            pythonKey: "api:setTempo1.example.python",
-            javascriptKey: "api:setTempo1.example.javascript",
+            tempo: "float",
         },
     }, {
         parameters: {
-            startTempo: {
-                typeKey: "api:types.float",
-            },
-            start: {
-                typeKey: "api:types.float",
-            },
-            endTempo: {
-                typeKey: "api:types.floatOptional",
-            },
-            end: {
-                typeKey: "api:types.floatOptional",
-            },
-        },
-        example: {
-            pythonKey: "api:setTempo2.example.python",
-            javascriptKey: "api:setTempo2.example.javascript",
+            startTempo: "float",
+            start: "float",
+            endTempo: "floatOptional",
+            end: "floatOptional",
         },
     }],
     shuffleList: [{
         parameters: {
-            list: {
-                typeKey: "api:types.listArray",
-            },
+            list: "listArray",
         },
         returns: "listArray",
-        example: {
-            pythonKey: "api:shuffleList.example.python",
-            javascriptKey: "api:shuffleList.example.javascript",
-        },
     }],
     shuffleString: [{
         parameters: {
-            string: {
-                typeKey: "api:types.string",
-            },
+            string: "string",
         },
         returns: "string",
-        example: {
-            pythonKey: "api:shuffleString.example.python",
-            javascriptKey: "api:shuffleString.example.javascript",
-        },
     }],
 }
 
-function getSignature(name: string, parameters: { [name: string]: Parameter }) {
+function getSignature(name: string, parameters: { [name: string]: { default?: string } }) {
     const paramStrings = Object.entries(parameters).map(
         ([param, info]) => param + (info.default ? `=${info.default}` : "")
     )
@@ -606,22 +328,32 @@ const apiDoc: { [key: string]: APIItem[] } = {}
 for (const [name, entries] of Object.entries(rawDoc)) {
     apiDoc[name] = entries.map((entry, i) => {
         const key = `api:${name}` + (entries.length > 1 ? (i + 1) : "")
-        const { signature, template } = getSignature(name, entry.parameters ?? {})
-        const parameters = entry.parameters && fromEntries(Object.entries(entry.parameters).map(([param, info]) => ([
-            param,
-            { ...info, descriptionKey: `${key}.parameters.${param}.description` },
-        ])))
+        const parameters = entry.parameters && fromEntries(Object.entries(entry.parameters).map(([param, info]) => {
+            const tmp = typeof info === "string" ? { type: info, default: undefined } : info
+            const expanded = {
+                typeKey: `api:types.${tmp.type}`,
+                descriptionKey: `${key}.parameters.${param}.description`,
+                default: tmp.default,
+            }
+            return [param, expanded]
+        }))
+        const { signature, template } = getSignature(name, parameters ?? {})
         const returns = entry.returns === undefined
             ? undefined
             : {
                 typeKey: `api:types.${entry.returns}`,
                 descriptionKey: `${key}.returns.description`,
             }
+        const example = {
+            pythonKey: entry.language === "javascript" ? "should not show" : `${key}.example.python`,
+            javascriptKey: entry.language === "python" ? "should not show" : `${key}.example.javascript`,
+        }
         return {
             ...entry,
             descriptionKey: key + ".description",
             parameters,
             returns,
+            example,
             signature,
             template,
         }
