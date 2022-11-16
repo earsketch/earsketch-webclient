d3d0121d78e68fd97de4c5fddf53ff2b01d77d02
Stephen Garrett <manodrum@gmail.com>
2022-11-16 13:09:09 -0500

    comment happypack

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@
  */
 const path = require("path")
 const webpack = require("webpack")
-const HappyPack = require("happypack")
+// const HappyPack = require("happypack")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
 
@@ -65,9 +65,9 @@ module.exports = {
                 path.resolve(__dirname, libDir),
                 path.resolve(__dirname, dataDir),
             ],
-            // loader: 'babel-loader',
-            // options: { presets: ['@babel/env'] }
-            use: "happypack/loader",
+            loader: "babel-loader",
+            options: { presets: ["@babel/env"] },
+            // use: "happypack/loader",
         }, {
             test: /\.(js|jsx|mjs)$/,
             use: "react-hot-loader/webpack",
@@ -118,10 +118,10 @@ module.exports = {
             createAudioMeter: "exports-loader?type=commonjs&exports=single createAudioMeter!volumeMeter",
             difflib: "exports-loader?type=commonjs&exports=single difflib!jsDiffLib",
         }),
-        new HappyPack({
-            threads: 4,
-            loaders: ["babel-loader?presets[]=@babel/env"],
-        }),
+        // new HappyPack({
+        //     threads: 4,
+        //     loaders: ["babel-loader?presets[]=@babel/env"],
+        // }),
         new HtmlWebpackPlugin({
             filename: path.resolve(distDir, "index.html"),
             template: "public/index.html",
