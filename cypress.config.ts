474120fabf82a73f4dc21039a4e9b885c88e7f51
heerman <lwheerman@gmail.com>
2022-11-30 12:31:57 -0500

    do not save failure videos

--- cypress.config.ts
+++ cypress.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
             inlineAssets: true,
         },
     },
+    video: false,
     e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
