8b0cff240029347c8072320bd59584d3b4f6dde9
Jas1238Smith <jas1238smith@gmail.com>
2022-12-08 21:34:54 -0500

    Weighted random for suggestAdvanceCode

--- src/cai/suggestAdvanceCode.ts
+++ src/cai/suggestAdvanceCode.ts
@@ -1,4 +1,4 @@
-import { SuggestionModule, curriculumProgression, suggestionHistory } from "./suggestionModule"
+import { suggestionHistory, SuggestionModule, SuggestionOptions, SuggestionContent, weightedRandom, addWeight, curriculumProgression } from "./suggestionModule"
 import { studentModel } from "./student"
 import { getApiCalls, ForNode, JsForNode, AugAssignNode } from "./complexityCalculator" // CodeFeatures
 import { CodeRecommendation } from "./codeRecommendations"
@@ -21,36 +21,50 @@ import { selectRegularScripts } from "../browser/scriptsState"
     - shorter code: if/else statement logic -> place in variables
 */
 
+const suggestionContent: SuggestionContent = {
+    function: { } as CodeRecommendation,
+    modularize: { } as CodeRecommendation,
+    loop: { } as CodeRecommendation,
+    step: { } as CodeRecommendation,
+}
+
 export const AdvanceCodeModule: SuggestionModule = {
     weight: 0,
     suggestion: () => {
         // printAccessibleData();
 
-        const possibleSuggestions: CodeRecommendation[] = []
+        const possibleSuggestions: SuggestionOptions = {}
 
         // check if there's any function in the code vs what sound complexity found
         // todo: check function lines for corresponding sections via SoundProfileLookup
         if (Object.keys(studentModel.musicAttributes.soundProfile).length > 1 && ccstate.userFunctionReturns.length === 0) {
-            possibleSuggestions.push(createSimpleSuggestion(0, "I think you should write a function with the section you have already created"))
+            suggestionContent.function = createSimpleSuggestion(0, "I think you should write a function with the section you have already created")
+            possibleSuggestions.function = addWeight(suggestionContent.function)
         }
 
         // check each user defined function if they are called
         for (const functionReturn of ccstate.userFunctionReturns) {
             if (functionReturn.calls.length === 0) {
-                possibleSuggestions.push(createSimpleSuggestion(0, "I think you can modularize your code by calling " + functionReturn.name + " at least once"))
+                suggestionContent.modularize = createSimpleSuggestion(0, "I think you can modularize your code by calling " + functionReturn.name + " at least once")
+                possibleSuggestions.modularize = addWeight(suggestionContent.modularize)
             }
         }
 
         // check for repeated code with fitMedia or makeBeat and suggest a loop
+        const loopRecommendations: CodeRecommendation[] = []
         let apiCalls = []
         apiCalls = Object.assign(getApiCalls(), [])
         apiCalls.sort((a, b) => (a.clips[0] <= b.clips[0] ? 1 : -1))
         apiCalls = apiCalls.filter((a) => { return a.function === "fitMedia" || a.function === "makeBeat" })
         for (let i = 2; i < apiCalls.length; i++) {
             if (apiCalls[i].clips[0] === apiCalls[i - 1].clips[0] && apiCalls[i].clips[0] === apiCalls[i - 2].clips[0]) {
-                possibleSuggestions.push(createSimpleSuggestion(0, "maybe try using a loop since you have a few lines using " + apiCalls[i].clips[0]))
+                loopRecommendations.push(createSimpleSuggestion(0, "maybe try using a loop since you have a few lines using " + apiCalls[i].clips[0]))
                 break
             }
+        }
+        if (loopRecommendations.length) {
+            suggestionContent.instrument = loopRecommendations[Math.floor(Math.random() * loopRecommendations.length)]
+            possibleSuggestions.instrument = addWeight(suggestionContent.instrument)
         }
 
         // TODO: needs count of where a variable is actually called, similar to custom function calls...
@@ -74,6 +88,8 @@ export const AdvanceCodeModule: SuggestionModule = {
 
         // TODO: increase loop score
         //      - loop + if/else range -> add a range to the loop (check for if/else in loop code, then check if 'i' is in logical condition)
+        const stepRecommendations: CodeRecommendation[] = []
+
         const scripts = Object.values(selectRegularScripts(store.getState()))
         const currentScript = scripts.find(({ name }) => name === caiState.selectActiveProject(store.getState()))
         const scriptType = currentScript?.name.slice(-2) === "js" ? "javascript" : "python"
@@ -90,18 +106,21 @@ export const AdvanceCodeModule: SuggestionModule = {
                 const assignComparisons = loop.body.filter(({ _astname }) => _astname === "AugAssign") as AugAssignNode[]
                 for (const aC of assignComparisons) {
                     if (aC.target._astname === "Name" && aC.target.id.v === comparison) {
-                        possibleSuggestions.push(createSimpleSuggestion(0, "you can add a step function since you change " + comparison + " on line " + aC.lineno))
+                        stepRecommendations.push(createSimpleSuggestion(0, "you can add a step function since you change " + comparison + " on line " + aC.lineno))
                     }
                 }
             }
         }
+        if (stepRecommendations.length) {
+            suggestionContent.step = stepRecommendations[Math.floor(Math.random() * stepRecommendations.length)]
+            possibleSuggestions.step = addWeight(suggestionContent.step)
+        }
 
         console.log("all suggestions: ", possibleSuggestions)
-        const loc = Math.floor(Math.random() * possibleSuggestions.length)
-        const suggestion = possibleSuggestions[loc]
-        console.log("picked suggestion: ", suggestion, loc)
-        suggestionHistory.push(suggestion)
-        return suggestion
+        const suggIndex = weightedRandom(possibleSuggestions)
+        console.log("picked suggestion: ", suggestionContent[suggIndex])
+        suggestionHistory.push(suggestionContent[suggIndex])
+        return suggestionContent[suggIndex]
     },
 }
 
