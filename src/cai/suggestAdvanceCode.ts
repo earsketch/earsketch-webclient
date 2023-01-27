e7a44121c7f867c47d8260f6d912a0e388ca5fc2
Erin J.K. Truesdell <nern.strudel@gmail.com>
2023-01-27 18:26:37 -0500

    "elif" fix

--- src/cai/suggestAdvanceCode.ts
+++ src/cai/suggestAdvanceCode.ts
@@ -27,41 +27,41 @@ const suggestionContent: SuggestionContent = {
         id: 214,
         utterance: "one of the things variables let us do is hold different values in different parts of our script",
         explain: "we already declare some variables, so if we want to put a different value in there later in the code, we can do that",
-        examplePY: "synth = HIPHOP_SYNTHPLUCKLEAD_005\n\n        fitMedia(synth, 1, 1, 2)\n        fitMedia(synth, 1, 3, 4)\n\n               synth = HIPHOP_SOLOMOOGLEAD_001\n\nfitMedia(synth, 1, 4, 5)\n        fitMedia(synth, 1, 2, 3)        ",
-        exampleJS: "var synth = HIPHOP_SYNTHPLUCKLEAD_005;\n\n        fitMedia(synth, 1, 1, 2);\n        fitMedia(synth, 1, 3, 4);\n\n               synth = HIPHOP_SOLOMOOGLEAD_001;\n\nfitMedia(synth, 1, 4, 5);\n        fitMedia(synth, 1, 2, 3);        ",
+        examplePY: "synth = HIPHOP_SYNTHPLUCKLEAD_005\n\n        [LINK|fitMedia](synth, 1, 1, 2)\n        [LINK|fitMedia](synth, 1, 3, 4)\n\n               synth = HIPHOP_SOLOMOOGLEAD_001\n\nfitMedia(synth, 1, 4, 5)\n        [LINK|fitMedia](synth, 1, 2, 3)        ",
+        exampleJS: "[LINK|var] synth = HIPHOP_SYNTHPLUCKLEAD_005;\n\n        [LINK|fitMedia](synth, 1, 1, 2);\n        [LINK|fitMedia](synth, 1, 3, 4);\n\n               synth = HIPHOP_SOLOMOOGLEAD_001;\n\nfitMedia(synth, 1, 4, 5);\n        [LINK|fitMedia](synth, 1, 2, 3);        ",
     },
     makeBeat2: {
         id: 215,
         utterance: "we could use the advanced [LINK|makeBeat] and add more sounds to our beat",
         explain: "the advanced [LINK|makeBeat] lets us create a beat with multiple samples by using list indexing",
-        examplePY: "drums = [OS_KICK05, OS_SNARE01]\n        beat = \"0+++1+++0+++1+++\"\n        makeBeat(drums, 1, 3, beat)",
-        exampleJS: "var drums = [OS_KICK05, OS_SNARE01];\n        var beat = \"0+++1+++0+++1+++\";\n        makeBeat(drums, 1, 3, beat);",
+        examplePY: "drums = [OS_KICK05, OS_SNARE01]\n        beat = \"0+++1+++0+++1+++\"\n        [LINK|makeBeat](drums, 1, 3, beat)",
+        exampleJS: "[LINK|var] drums = [OS_KICK05, OS_SNARE01];\n        var beat = \"0+++1+++0+++1+++\";\n        [LINK|makeBeat](drums, 1, 3, beat);",
     },
     forLoopsRange2: {
         id: 216,
         utterance: "we could add a minimum value to our range()",
         explain: "that would give us a little more control over what the loop does by giving it a start and end value",
-        examplePY: "for measure in range(1, 5):\n         makeBeat(OS_SNARE03, 1, measure, \"0---0---0-0-0---\")",
+        examplePY: "[LINK|for] measure in range(1, 5):\n         [LINK|makeBeat](OS_SNARE03, 1, measure, \"0---0---0-0-0---\")",
     },
     forLoopsRange3: {
         id: 217,
         utterance: "we could add a step value to our range() to skip values if we want",
         explain: "we can do something like putting sounds in every other measure or every third measure",
-        examplePY: "for measure in range(1, 9, 2):\n         makeBeat(OS_SNARE03, 1, measure, \"0---0---0-0-0---\")",
+        examplePY: "[LINK|for] measure in range(1, 9, 2):\n         [LINK|makeBeat](OS_SNARE03, 1, measure, \"0---0---0-0-0---\")",
     },
     conditionals2: {
         id: 218,
         utterance: "let's add an else portion for our [LINK|conditional] to do something if the condition is FALSE",
         explain: "this means we can have the code do something no matter what the condition works out to",
-        examplePY: "a = readInput(\"Do you like hip-hop music? Yes/No.\")\n        \n\nif (a == \"yes\" or a == \"Yes\" or a == \"YES\"):\n      print(\"Hip-hop it is!\") \n    fitMedia(YG_NEW_HIP_HOP_ARP_1, 1, 1, 9)        \nelse:    \n     print(\"Ok, here is some funk.\")    \n fitMedia(YG_NEW_FUNK_ELECTRIC_PIANO_4, 1, 1, 9)",
-        exampleJS: "var a = readInput(\"Do you like hip-hop music? Yes/No.\");\n        \n\nif (a == \"yes\" or a == \"Yes\" or a == \"YES\") {\n      println(\"Hip-hop it is!\"); \n    fitMedia(YG_NEW_HIP_HOP_ARP_1, 1, 1, 9);\n} else {    \n     println(\"Ok, here is some funk.\");   \n fitMedia(YG_NEW_FUNK_ELECTRIC_PIANO_4, 1, 1, 9);\n}",
+        examplePY: "a = [LINK|readInput](\"Do you like hip-hop music? Yes/No.\")\n        \n\n[LINK|if] (a == \"yes\" or a == \"Yes\" or a == \"YES\"):\n      print(\"Hip-hop it is!\") \n    [LINK|fitMedia](YG_NEW_HIP_HOP_ARP_1, 1, 1, 9)        \nelse:    \n     print(\"Ok, here is some funk.\")    \n fitMedia(YG_NEW_FUNK_ELECTRIC_PIANO_4, 1, 1, 9)",
+        exampleJS: "[LINK|var] a = [LINK|readInput](\"Do you like hip-hop music? Yes/No.\");\n        \n\n[LINK|if] (a == \"yes\" or a == \"Yes\" or a == \"YES\") {\n      println(\"Hip-hop it is!\"); \n    [LINK|fitMedia](YG_NEW_HIP_HOP_ARP_1, 1, 1, 9);\n} else {    \n     println(\"Ok, here is some funk.\");   \n fitMedia(YG_NEW_FUNK_ELECTRIC_PIANO_4, 1, 1, 9);\n}",
     },
     conditionals3: {
         id: 219,
-        utterance: "we can also add \"" + (parseLanguage(selectActiveProject(store.getState())) === "python" ? "elif" : "else if") + "\" portions to follow more than two paths based on the conditions",
+        utterance: "we can also add \"[ELIF]\" portions to follow more than two paths based on the conditions",
         explain: "with this we can have the code do more than two paths within our one conditional statement",
-        examplePY: "a = readInput(\"Do you like hip-hop music? Yes/No.\")\n        \n\nif (a == \"yes\" or a == \"Yes\" or a == \"YES\"):\n      print(\"Hip-hop it is!\") \n    fitMedia(YG_NEW_HIP_HOP_ARP_1, 1, 1, 9)        \nelif (a == \"no\" or a == \"No\" or a == \"NO\"):    \n     print(\"Ok, here is some funk.\")    \n fitMedia(YG_NEW_FUNK_ELECTRIC_PIANO_4, 1, 1, 9)\n else:\n     print(\"Sorry, I didn't get that. Please enter Yes or No.\")",
-        exampleJS: "var a = readInput(\"Do you like hip-hop music? Yes/No.\");\n        \n\nif (a == \"yes\" or a == \"Yes\" or a == \"YES\") {\n      println(\"Hip-hop it is!\");\n    fitMedia(YG_NEW_HIP_HOP_ARP_1, 1, 1, 9);\n} else if (a == \"no\" or a == \"No\" or a == \"NO\") {    \n     println(\"Ok, here is some funk.\");    \n fitMedia(YG_NEW_FUNK_ELECTRIC_PIANO_4, 1, 1, 9);\n} else {\n     println(\"Sorry, I didn't get that. Please enter Yes or No.\");\n}",
+        examplePY: "a = [LINK|readInput](\"Do you like hip-hop music? Yes/No.\")\n        \n\n[LINK|if] (a == \"yes\" or a == \"Yes\" or a == \"YES\"):\n      print(\"Hip-hop it is!\") \n    [LINK|fitMedia](YG_NEW_HIP_HOP_ARP_1, 1, 1, 9)        \nelif (a == \"no\" or a == \"No\" or a == \"NO\"):    \n     print(\"Ok, here is some funk.\")    \n [LINK|fitMedia](YG_NEW_FUNK_ELECTRIC_PIANO_4, 1, 1, 9)\n else:\n     print(\"Sorry, I didn't get that. Please enter Yes or No.\")",
+        exampleJS: "[LINK|var] a = [LINK|readInput](\"Do you like hip-hop music? Yes/No.\");\n        \n\n[LINK|if] (a == \"yes\" or a == \"Yes\" or a == \"YES\") {\n      println(\"Hip-hop it is!\");\n    [LINK|fitMedia](YG_NEW_HIP_HOP_ARP_1, 1, 1, 9);\n} else if (a == \"no\" or a == \"No\" or a == \"NO\") {    \n     println(\"Ok, here is some funk.\");    \n [LINK|fitMedia](YG_NEW_FUNK_ELECTRIC_PIANO_4, 1, 1, 9);\n} else {\n     println(\"Sorry, I didn't get that. Please enter Yes or No.\");\n}",
     },
     repeatExecution2: {
         id: 220,
@@ -71,8 +71,8 @@ const suggestionContent: SuggestionContent = {
         id: 221,
         utterance: "let's use function [LINK|parameters] so we can use our function to do similar things but not always exactly the same thing",
         explain: "using parameters means we can specify some things about the function when we call it, like using different sounds in fitMedia calls",
-        examplePY: "def sectionA(startMeasure, endMeasure):\n        fitMedia(RD_WORLD_PERCUSSION_KALIMBA_PIANO_7, 1, startMeasure, endMeasure)\n        fitMedia(RD_WORLD_PERCUSSION_DRUMPART_24, 2, startMeasure, endMeasure)  \n\n    sectionA(1, 5)\n    sectionA(9, 13)",
-        exampleJS: "function sectionA(startMeasure, endMeasure) {\n        fitMedia(RD_WORLD_PERCUSSION_KALIMBA_PIANO_7, 1, startMeasure, endMeasure);\n        fitMedia(RD_WORLD_PERCUSSION_DRUMPART_24, 2, startMeasure, endMeasure); \n}  \n\n    sectionA(1, 5);\n    sectionA(9, 13);",
+        examplePY: "[LINK|def] sectionA(startMeasure, endMeasure):\n        [LINK|fitMedia](RD_WORLD_PERCUSSION_KALIMBA_PIANO_7, 1, startMeasure, endMeasure)\n        [LINK|fitMedia](RD_WORLD_PERCUSSION_DRUMPART_24, 2, startMeasure, endMeasure)  \n\n    sectionA(1, 5)\n    sectionA(9, 13)",
+        exampleJS: "[LINK|function] sectionA(startMeasure, endMeasure) {\n        [LINK|fitMedia](RD_WORLD_PERCUSSION_KALIMBA_PIANO_7, 1, startMeasure, endMeasure);\n        [LINK|fitMedia](RD_WORLD_PERCUSSION_DRUMPART_24, 2, startMeasure, endMeasure); \n}  \n\n    sectionA(1, 5);\n    sectionA(9, 13);",
     },
     manipulateValue2: {
         id: 222,
@@ -82,8 +82,8 @@ const suggestionContent: SuggestionContent = {
         id: 223,
         utterance: "we can also use our returned value somewhere",
         explain: "since we return a value, we can use that as input for something else, like using the end measure returned from one section as the start measure for the next section",
-        examplePY: "def createBeat(startMeasure, soundClip, beatString):\n        endMeasure = startMeasure + 3\n        for measure in range(startMeasure, endMeasure):\n            makeBeat(soundClip, 1, measure, beatString)\n\n        # Return ending measure so we can use it outside function\n        return endMeasure",
-        exampleJS: "function createBeat(startMeasure, soundClip, beatString) {\n        var endMeasure = startMeasure + 3;\n        for (i = startMeasure; i <= endMeasure; i++) {\n            makeBeat(soundClip, 1, measure, beatString);\n}\n\n        # Return ending measure so we can use it outside function\n        return endMeasure\n}",
+        examplePY: "[LINK|def] createBeat(startMeasure, soundClip, beatString):\n        endMeasure = startMeasure + 3\n        [LINK|for] measure in range(startMeasure, endMeasure):\n            [LINK|makeBeat](soundClip, 1, measure, beatString)\n\n        # Return ending measure so we can use it outside function\n        return endMeasure",
+        exampleJS: "[LINK|function] createBeat(startMeasure, soundClip, beatString) {\n        [LINK|var] endMeasure = startMeasure + 3;\n        [LINK|for] (i = startMeasure; i <= endMeasure; i++) {\n            [LINK|makeBeat](soundClip, 1, measure, beatString);\n}\n\n        # Return ending measure so we can use it outside function\n        return endMeasure\n}",
     },
 }
 
@@ -91,6 +91,7 @@ export const AdvanceCodeModule: SuggestionModule = {
     weight: 0,
     suggestion: () => {
         // printAccessibleData();
+        const lang = parseLanguage(selectActiveProject(store.getState()))
         const state = store.getState()
         const currentScript = selectActiveTabScript(state)
         const activeProject = selectActiveProject(state)
@@ -248,8 +249,14 @@ export const AdvanceCodeModule: SuggestionModule = {
             suggestionContent.function = createSimpleSuggestion(0, "let's look for ways to modularize the current code")
             possibleSuggestions.function = addWeight(suggestionContent.function)
         }
-        const suggIndex = weightedRandom(possibleSuggestions)
-        return suggestionContent[suggIndex]
+        // const suggIndex = weightedRandom(possibleSuggestions)
+        const suggIndex = "conditionals3"
+        const suggObj = Object.assign({}, suggestionContent[suggIndex])
+        if (suggObj.utterance.includes("[ELIF]")) {
+            const elseIndex = suggObj.utterance.indexOf("[ELIF]")
+            suggObj.utterance = suggObj.utterance.substring(0, elseIndex) + (lang === "python" ? "elif" : "else if") + suggObj.utterance.substring(elseIndex + 6)
+        }
+        return suggObj
     },
 }
 
