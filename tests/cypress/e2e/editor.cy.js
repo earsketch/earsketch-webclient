62cdf2e72848b4b660e2e60cfe314785f1b9a928
Stephen Garrett <manodrum@gmail.com>
2023-02-02 16:03:40 -0500

    chain `.click()`, remove commented code

--- tests/cypress/e2e/editor.cy.js
+++ tests/cypress/e2e/editor.cy.js
@@ -43,9 +43,8 @@ describe("Editor", () => {
     it("toggles autocomplete off", () => {
         cy.get("#editor").type("{moveToEnd}{enter}f")
         cy.get(".cm-tooltip-autocomplete").should("be.visible")
-        cy.get(".cm-tooltip-autocomplete > ul").find("li[aria-selected='true']").contains("fitMedia").should("be.visible")
-        cy.get(".cm-tooltip-autocomplete > ul").find("li[aria-selected='true']").contains("fitMedia").click()
-        // cy.realType("{enter}")
+        cy.get(".cm-tooltip-autocomplete > ul").find("li[aria-selected='true']")
+            .contains("fitMedia").should("be.visible").click()
         cy.realType("OS_CLAP01")
         cy.get(".cm-line").contains("fitMedia(OS_CLAP01,")
         cy.get("button[title='Editor Settings']").click()
