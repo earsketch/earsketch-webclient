c6d1d09b0ffa814ac3e81d15e1127ab51b44dc99
Ian Clester <ijc@ijc8.me>
2023-03-31 11:59:49 -0400

    Apply lower bound to `startMes`, too
    May want to revisit DAW behavior upon loading another project; also
    should consider behavior when running a new, shorter script while
    playing (e.g. if there's a loop selection beyond the bounds of the new
    project length).

--- src/audio/player.ts
+++ src/audio/player.ts
@@ -58,9 +58,10 @@ export interface TrackGraph {
 }
 
 export function play(startMes: number, delay = 0) {
+    const minStartMes = (loop.on && loop.selection) ? loop.start : 1
     const endMes = (loop.on && loop.selection) ? loop.end : dawData!.length + 1
-    if (startMes >= endMes) {
-        startMes = (loop.on && loop.selection) ? loop.start : 1
+    if (startMes < minStartMes || startMes >= endMes) {
+        startMes = minStartMes
     }
     const tempoMap = new TempoMap(dawData!)
     const startTime = tempoMap.measureToTime(startMes)
