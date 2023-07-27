8b6098db063d6b2a045c38dc5c3f183abf466321
mneedham01 <mneedham@smith.edu>
2023-07-27 14:02:52 -0400

    attempted to add python/javascript differentiation

--- src/api/api.ts
+++ src/api/api.ts
@@ -374,6 +374,10 @@ for (const [name, entries] of Object.entries(rawDoc)) {
                 typeKey: `api:types.${entry.returns}`,
                 descriptionKey: `${key}.returns.description`,
             }
+        const values = {
+            pythonKey: entry.language === "javascript" ? "should not show" : `${key}.listArray.python`,
+            javascriptKey: entry.language === "python" ? "should not show" : `${key}.listArray.javascript`,
+        }
         const example = {
             pythonKey: entry.language === "javascript" ? "should not show" : `${key}.example.python`,
             javascriptKey: entry.language === "python" ? "should not show" : `${key}.example.javascript`,
@@ -391,3 +395,4 @@ for (const [name, entries] of Object.entries(rawDoc)) {
 }
 
 export const API_DOC: { readonly [key: string]: readonly APIItem[] } = apiDoc
+
