e23a754038ec748d346bd009197c801b67f2508b
mneedham01 <mneedham@smith.edu>
2023-07-31 10:56:16 -0400

    deleted console.log

--- src/app/postRun.ts
+++ src/app/postRun.ts
@@ -40,8 +40,6 @@ export async function postRun(result: DAWData) {
     // STEP 5: Insert metronome as the first track.
     esconsole("Adding metronome track.", ["debug", "runner"])
     await addMetronome(result)
-
-    console.log(result)
 }
 
 export async function loadBuffersForSampleSlicing(result: DAWData) {
