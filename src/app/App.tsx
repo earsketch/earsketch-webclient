5b54bb18b6e8bdca4e3f1aa08ab69ee1903e0df6
heerman <lwheerman@gmail.com>
2023-08-07 10:52:42 -0400

    remove shortcut as suggestions as now automatic

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -459,7 +459,6 @@ const KeyboardShortcuts = () => {
         undo: [modifier, "Z"],
         redo: [modifier, "Shift", "Z"],
         comment: [modifier, "/"],
-        autocomplete: ["Enter"],
         zoomHorizontal: <>
             <kbd>{modifier}</kbd>+<kbd>{localize("Wheel")}</kbd> or <kbd>+</kbd>/<kbd>-</kbd>
         </>,
