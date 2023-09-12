24148ff0b46bcf8e1a07da9811fba44d3e6f07dc
heerman <lwheerman@gmail.com>
2023-09-12 11:34:35 -0400

    show js error directly

--- src/ide/Editor.tsx
+++ src/ide/Editor.tsx
@@ -542,9 +542,10 @@ export const Editor = ({ importScript }: { importScript: (s: Script) => void }) 
             droplet.on("change", () => setContents(droplet.getValue(), undefined, false))
         } else {
             dispatch(setBlocksMode(false))
-            const msg = result.error.message.includes("ParseError is not a constructor")
+            let msg = result.error.message.includes("ParseError is not a constructor")
                 ? t("messages:idecontroller.blocksParseError")
                 : t("messages:idecontroller.blocksError")
+            if (language === "javascript") { msg = result.error.toString() }
             userNotification.showBanner(msg, "failure1")
         }
     }
