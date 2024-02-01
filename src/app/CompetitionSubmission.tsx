8d88617617db2f2459cef0eea89f02fc053a36d9
Stephen Garrett <manodrum@gmail.com>
2024-02-01 09:54:02 -0500

    update rocketjudge link and flags

--- src/app/CompetitionSubmission.tsx
+++ src/app/CompetitionSubmission.tsx
@@ -23,7 +23,7 @@ export const CompetitionSubmission = ({ name, shareID, close }: { name: string, 
                     </ul>
                 </div>
                 <div className="text-center m-auto">
-                    <a href={"https://www.rocketjudge.com/register/JOoVvGIo#scriptid=".concat(shareID)} target="_blank" rel="noreferrer">
+                    <a href={"https://www.rocketjudge.com/register/Jg8UUOFs#scriptid=".concat(shareID)} target="_blank" rel="noreferrer">
                         <button style={{ minWidth: "270px", fontSize: "26px", color: "black", background: "#d3d25a" }}>Click Here to Submit</button>
                     </a>
                 </div>
