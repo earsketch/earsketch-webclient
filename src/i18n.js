4a7500308ab698fd3650bf54b05b01701eee2760
Stephen Garrett <manodrum@gmail.com>
2024-05-13 13:00:59 -0400

    add Uzbek to i18next locales

--- src/i18n.js
+++ src/i18n.js
@@ -5,6 +5,7 @@ import es from "./locales/es/common.json"
 import fr from "./locales/fr/common.json"
 import iu from "./locales/iu/common.json"
 import oj from "./locales/oj/common.json"
+import uz from "./locales/uz/common.json"
 
 i18n
     .use(initReactI18next) // passes i18n down to react-i18next
@@ -25,6 +26,7 @@ i18n
             fr,
             iu,
             oj,
+            uz,
         },
     })
 
