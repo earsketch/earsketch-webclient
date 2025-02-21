e6ac0d99addc9a2ca0a39f524ad811eea32f9c23
Sangheon Park <michaelpark1102@gmail.com>
2025-02-21 12:20:37 -0500

    testing removing tick

--- src/audio/renderer.ts
+++ src/audio/renderer.ts
@@ -14,7 +14,7 @@ export async function renderBuffer(dawData: DAWData) {
     esconsole("Begin rendering result to buffer.", ["debug", "renderer"])
 
     const tempoMap = new TempoMap(dawData)
-    const endOfSongPadding = 3
+    const endOfSongPadding = 1 // + 1 
     const duration = tempoMap.measureToTime(dawData.length + 1) + endOfSongPadding // need +1 to render to end of last measure
     const context = new OfflineAudioContext(NUM_CHANNELS, SAMPLE_RATE * duration, SAMPLE_RATE)
     await context.audioWorklet.addModule(pitchshiftWorkletURL)
@@ -33,11 +33,12 @@ export async function renderBuffer(dawData: DAWData) {
         }
     }
 
-    const fadeOutDuration = 0.5; 
+    const fadeOutDuration = 0.4; 
     const fadeOutStartTime = duration - fadeOutDuration;
 
     out.gain.setValueAtTime(1, fadeOutStartTime);
-    out.gain.linearRampToValueAtTime(0, duration); 
+    out.gain.linearRampToValueAtTime(0.001, duration); //testing 
+    out.gain.setValueAtTime(0, duration); 
 
     const buffer = await context.startRendering()
     esconsole("Render to buffer completed.", ["debug", "renderer"])
