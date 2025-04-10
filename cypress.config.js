f99cc22f9db50049d8f04b14f9ce44b4d28ef93c
Stephen Garrett <manodrum@gmail.com>
2025-04-10 10:49:09 -0400

    initial commit, cypress components working w vite

--- cypress.config.js
+++ cypress.config.js
@@ -1,32 +1,44 @@
-import { defineConfig } from "cypress"
-import mochawsomePlugin from "cypress-mochawesome-reporter/plugin"
+import { defineConfig } from "cypress";
+import mochawsomePlugin from "cypress-mochawesome-reporter/plugin";
 
 export default defineConfig({
-    fixturesFolder: "tests/cypress/fixtures",
-    screenshotsFolder: "tests/cypress/screenshots",
-    videosFolder: "tests/cypress/videos",
-    downloadsFolder: "tests/cypress/downloads",
-    reporter: "cypress-multi-reporters",
-    reporterOptions: {
-        reporterEnabled: "spec, cypress-mochawesome-reporter",
-        cypressMochawesomeReporterReporterOptions: {
-            reportDir: "tests/cypress/reports",
-            charts: true,
-            reportTitle: "EarSketch E2E Test Suite",
-            reportPageTitle: "EarSketch E2E Test Suite",
-            embeddedScreenshots: true,
-            inlineAssets: true,
-        },
+  fixturesFolder: "tests/cypress/fixtures",
+  screenshotsFolder: "tests/cypress/screenshots",
+  videosFolder: "tests/cypress/videos",
+  downloadsFolder: "tests/cypress/downloads",
+  reporter: "cypress-multi-reporters",
+
+  reporterOptions: {
+    reporterEnabled: "spec, cypress-mochawesome-reporter",
+    cypressMochawesomeReporterReporterOptions: {
+      reportDir: "tests/cypress/reports",
+      charts: true,
+      reportTitle: "EarSketch E2E Test Suite",
+      reportPageTitle: "EarSketch E2E Test Suite",
+      embeddedScreenshots: true,
+      inlineAssets: true,
     },
-    video: false,
-    e2e: {
+  },
+
+  video: false,
+
+  e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-        setupNodeEvents(on, config) {
-            mochawsomePlugin(on)
-        },
-        baseUrl: "http://localhost:8888",
-        specPattern: "tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
-        supportFile: "tests/cypress/support/index.js",
+    setupNodeEvents(on, config) {
+      mochawsomePlugin(on);
     },
-})
+    baseUrl: "http://localhost:8888",
+    specPattern: "tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "tests/cypress/support/index.js",
+  },
+
+  component: {
+    supportFile: "tests/cypress/support/component.js",
+    indexHtmlFile: "tests/cypress/support/component-index.html",
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+});
---
+++ tests/cypress/component/curriculum.scripts.cy.jsx
@@ -0,0 +1,15 @@
+import * as ESUtils from "../../../src/esutils"
+import * as runner from "../../../src/app/runner"
+
+import { customMatchers } from "./setup"
+import { CURRICULUM_SCRIPTS } from "./curriculum.scripts"
+import { CURRICULUM_RESULTS } from "./curriculum.results"
+import { CURRICULUM_V2_SCRIPTS } from "./curriculum-v2.scripts"
+import { CURRICULUM_V2_RESULTS } from "./curriculum-v2.results"
+
+describe("curriculum.scripts.cy.jsx", () => {
+    it("playground", () => {
+    // cy.mount()
+
+    })
+})
