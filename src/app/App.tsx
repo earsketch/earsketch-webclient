246016c04d7f0c3f28d76c0274ce8810a71e2c8b
Luke Heerman <43624284+heerman@users.noreply.github.com>
2025-05-30 14:14:14 -0400

    Put past banners into components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -452,6 +452,46 @@ function forgotPass() {
     openModal(ForgotPassword)
 }
 
+const HeaderBanner = ({ banner }: { banner: "earsketch-competition" | "summit" }) => {
+    if (banner === "earsketch-competition") {
+        return <EarSketchTeachersCompetitionBanner />
+    } else if (banner === "summit") {
+        return <EarSketchSummitBanner />
+    }
+    return null
+}
+
+const EarSketchTeachersCompetitionBanner = () => {
+    return (<div className="hidden w-full lg:flex justify-evenly">
+        <a href="https://www.teachers.earsketch.org/compete"
+            aria-label="Link to the competition website"
+            target="_blank"
+            className="text-black uppercase dark:text-white text-center"
+            style={{ color: "yellow", textShadow: "1px 1px #FF0000", lineHeight: "21px", fontSize: "18px" }}
+            rel="noreferrer">
+            <div className="flex flex-col items-center">
+                <img style={{ height: "20px" }} src={teachersLogo} id="comp-logo" alt="Link to the competition site"/>
+                <div>Remix Competition</div>
+            </div>
+        </a>
+    </div>)
+}
+
+const EarSketchSummitBanner = () => {
+    return (<div className="hidden w-full lg:flex justify-evenly">
+        <a href="https://gatech.zoom.us/webinar/register/7917465553949/WN_3Z4_z1OHR_2NexLYdccNvA"
+            aria-label="Link to EarSketch SUMMIT Registration"
+            target="_blank"
+            className="text-center"
+            rel="noreferrer">
+            <div className="flex flex-col items-center">
+                <div className="text-amber">JOIN US AT THE EARSKETCH SUMMIT</div>
+                <div className="text-gray-200 text-xs">MAY 21 &bull; 10AM-12PM ET</div>
+            </div>
+        </a>
+    </div>)
+}
+
 const KeyboardShortcuts = () => {
     const isMac = ESUtils.whichOS() === "MacOS"
     const modifier = isMac ? "Cmd" : "Ctrl"
@@ -942,37 +982,7 @@ export const App = () => {
                         <h1 className="text-2xl text-white">EarSketch</h1>
                     </a>
                     <ConfettiLauncher/>
-                    {showAfeCompetitionBanner &&
-                        <div className="hidden w-full lg:flex justify-evenly">
-                            <a href="https://www.teachers.earsketch.org/compete"
-                                aria-label="Link to the competition website"
-                                target="_blank"
-                                className="text-black uppercase dark:text-white text-center"
-                                style={{
-                                    color: "yellow",
-                                    textShadow: "1px 1px #FF0000",
-                                    lineHeight: "21px",
-                                    fontSize: "18px",
-                                }}
-                                rel="noreferrer">
-                                <div className="flex flex-col items-center">
-                                    <img style={{ height: "20px" }} src={teachersLogo} id="comp-logo"
-                                        alt="Link to the competition site"/>
-                                    <div>Remix Competition</div>
-                                </div>
-                            </a>
-                        </div>}
-                    <div className="hidden w-full lg:flex justify-evenly">
-                        <a href="https://gatech.zoom.us/webinar/register/7917465553949/WN_3Z4_z1OHR_2NexLYdccNvA"
-                            aria-label="Link to EarSketch SUMMIT Registration"
-                            target="_blank"
-                            className="text-center" rel="noreferrer">
-                            <div className="flex flex-col items-center">
-                                <div className="text-amber">JOIN US AT THE EARSKETCH SUMMIT</div>
-                                <div className="text-gray-200 text-xs">MAY 21 &bull; 10AM-12PM ET</div>
-                            </div>
-                        </a>
-                    </div>
+                    {showAfeCompetitionBanner && <HeaderBanner banner="earsketch-competition" />}
                 </div>
 
                 {/* temporary place for the app-generated notifications */}
