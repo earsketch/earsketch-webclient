bd191c7752484637dc166e1be1d75fa19a101342
Ian Clester <ijc@ijc8.me>
2025-06-10 09:45:01 -0400

    Remove unnecessary declaration

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -17,7 +17,6 @@ declare const ES_WEB_UPLOAD_CAI_HISTORY: boolean
 declare const ES_WEB_SHOW_CHAT: boolean
 declare const ES_WEB_SHOW_LOCALE_SWITCHER: boolean
 
-declare const Hilitor: any
 declare const Kali: any
 
 declare module "d3"
