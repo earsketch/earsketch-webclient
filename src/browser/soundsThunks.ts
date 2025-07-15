f9c45468364f5f923f70489bae2eda969670ac57
Luke Heerman <43624284+heerman@users.noreply.github.com>
2025-07-15 11:38:16 -0400

    Remove username parameter from thunk

--- src/browser/soundsThunks.ts
+++ src/browser/soundsThunks.ts
@@ -40,10 +40,10 @@ export const getStandardSounds = createAsyncThunk<void, void, ThunkAPI>(
     }
 )
 
-export const getUserSounds = createAsyncThunk<void, string, ThunkAPI>(
+export const getUserSounds = createAsyncThunk<void, void, ThunkAPI>(
     "sounds/getUserSounds",
-    async (username, { dispatch }) => {
-        const data = await audioLibrary.getUserSounds(username)
+    async (_, { dispatch }) => {
+        const data = await audioLibrary.getUserSounds()
         const entities: { [key: string]: SoundEntity; } = {}
         const names = new Array(data.length)
         for (const [i, sound] of data.entries()) {
