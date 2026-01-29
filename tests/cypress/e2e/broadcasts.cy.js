6c98a357a971006b4298dd71713395a69c4b6bfd
Luke Heerman <43624284+heerman@users.noreply.github.com>
2026-01-29 09:13:14 -0500

    Test for broadcast notification on login

--- tests/cypress/e2e/broadcasts.cy.js
+++ tests/cypress/e2e/broadcasts.cy.js
@@ -1,13 +1,15 @@
-import { createServer } from "../support/wsServer"
 import * as MockSocket from "mock-socket"
 
-describe("broadcast notification", () => {
-    const wsServer = createServer("wss://api-dev.ersktch.gatech.edu/EarSketchWS/socket/cypress/")
-
-    it("is received", () => {
+describe("Notifications", () => {
+    it("delivers broadcast on login", () => {
         cy.interceptAudioStandard()
         cy.interceptUsersToken()
         cy.interceptUsersInfo()
+        cy.interceptUsersNotifications([{
+            notification_type: "broadcast",
+            username: "user2",
+            message: { text: "Hello, EarSketch!", hyperlink: "", expiration: 7 },
+        }])
         cy.interceptAudioUser()
         cy.interceptAudioFavorites()
         cy.interceptScriptsOwned()
@@ -17,19 +19,6 @@ describe("broadcast notification", () => {
         cy.skipTour()
         cy.login()
 
-        const msgForAll = "Hello, EarSketch!"
-
-        cy.incomingWebSocketMessage(
-            wsServer,
-            {
-                notification_type: "broadcast",
-                username: "user2",
-                message: { text: msgForAll, hyperlink: "", expiration: 13 },
-            }
-        )
-
-        cy.get("[data-test='numUnreadNotifications']").contains("2")
-
-        cy.get("div").contains("From EarSketch team: " + msgForAll)
+        cy.get("[data-test='numUnreadNotifications']").contains("1")
     })
 })
