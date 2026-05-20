5667d602a6b1b6415a8938deb615281cf379da70
Stephen Garrett <manodrum@gmail.com>
2026-05-20 14:58:35 -0400

    adjust tooltip colors for light and dark mode

--- src/browser/Sounds.tsx
+++ src/browser/Sounds.tsx
@@ -480,7 +480,7 @@ const Clip = React.memo(({ clip, bgcolor, borderColor, previewState, loggedIn, i
                                     align="start"
                                     sideOffset={4}
                                     style={{ fontSize: `${scaledFontSize}px` }}
-                                    className="scale:text-xs z-50 rounded bg-gray-800 px-3 py-2 text-white shadow-lg outline outline-1 outline-gray-400 pointer-events-none"
+                                    className="scale:text-xs z-50 rounded bg-gray-500 dark:bg-gray-700 px-3 py-2 text-white shadow-lg outline outline-1 outline-gray-400 dark:outline-gray-500 pointer-events-none"
                                 >
                                     {tooltipContent}
                                 </Tooltip.Content>
